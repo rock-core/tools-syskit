1b2c858cdc509b55fa368c746e103b792dd18f5e
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2009-12-09 11:54:22 +0100

    roby,test: move to common setup/teardown code that is now in test/roby/common

--- test/test_proxies.rb
+++ test/test_proxies.rb
@@ -1,48 +1,13 @@
-BASE_DIR = File.expand_path( '..', File.dirname(__FILE__))
-$LOAD_PATH.unshift BASE_DIR
-require 'test/unit'
-require 'roby'
-require 'roby/test/common'
-require 'roby/test/testcase'
-require 'roby/test/tasks/simple_task'
-require 'orocos/roby/app'
-require 'orocos/roby'
-
+BASE_DIR = File.expand_path( '../..', File.dirname(__FILE__))
 APP_DIR = File.join(BASE_DIR, "test")
 
+$LOAD_PATH.unshift BASE_DIR
+require 'test/roby/common'
+
 class TC_RobyPlugin_Proxies < Test::Unit::TestCase
-    include Roby::Test
-    include Roby::Test::Assertions
+    include RobyPluginCommonTest
 
-    WORK_DIR = File.join(BASE_DIR, '..', 'test', 'working_copy')
-    def setup
-        super
-
-        ::Orocos.initialize
-        Roby.app.extend Orocos::RobyPlugin::Application
-        save_collection Roby.app.loaded_orogen_projects
-        save_collection Roby.app.orocos_tasks
-        save_collection Roby.app.orocos_deployments
-
-        @update_handler = engine.each_cycle(&Orocos::RobyPlugin.method(:update))
-
-        FileUtils.mkdir_p Roby.app.log_dir
-        @old_pkg_config = ENV['PKG_CONFIG_PATH'].dup
-        ENV['PKG_CONFIG_PATH'] += ":#{File.join(WORK_DIR, "prefix", 'lib', 'pkgconfig')}"
-
-        Orocos::RobyPlugin::Application.setup
-    end
-
-    def teardown
-        Roby.app.orocos_clear_models
-        ::Orocos.instance_variable_set :@registry, Typelib::Registry.new
-        ::Orocos::CORBA.instance_variable_set :@loaded_toolkits, []
-        ENV['PKG_CONFIG_PATH'] = @old_pkg_config
-
-        FileUtils.rm_rf Roby.app.log_dir
-
-        super
-    end
+    needs_no_orogen_projects
 
     def test_deployment_nominal_actions
         Roby.app.load_orogen_project "echo"
