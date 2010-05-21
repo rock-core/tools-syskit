422cdc73fa1997f5edadbd9856521a3124dd7b1f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-05-21 16:10:47 +0200

    roby: add an -r robot_name,robot_type option to load_models
    It makes load_models more in line with the other scripts

--- lib/syskit/scripts/load_models.rb
+++ lib/syskit/scripts/load_models.rb
@@ -6,9 +6,14 @@ require 'orocos/roby/app'
 
 parser = OptionParser.new do |opt|
     opt.banner = <<-EOD
-Usage: scripts/orocos/load_models [robot_name]
-Verifies that all the models referred to by robot_name load fine
+Usage: scripts/orocos/load_models [options]
+Loads model files to check if there is no errors in them
     EOD
+
+    opt.on('-r NAME', '--robot=NAME[,TYPE]', String, 'the robot name whose models should be loaded') do |name|
+        robot_name, robot_type = name.split(',')
+        Roby.app.robot(name, robot_type||robot_name)
+    end
 
     opt.on_tail('-h', '--help', 'this help message') do
 	STDERR.puts opt
@@ -16,16 +21,9 @@ Verifies that all the models referred to by robot_name load fine
     end
 end
 
-robot_name = parser.parse(ARGV)
-if !robot_name.empty?
-    Roby.app.robot(robot_name.first)
-end
-
 Orocos::RobyPlugin.logger.level = Logger::INFO
 Roby.filter_backtrace do
     Roby.app.setup
 end
 STDERR.puts "all models load fine"
-
-
 
