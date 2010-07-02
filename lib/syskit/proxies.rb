23e5e5ad12875d7d756293e0ebe852dae0b651a8
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-07-02 21:29:48 +0200

    roby: do not stop the tasks when we quit the process
    Roby will by itself kill things in order. If one wants to kill
    a deployment, there might be a reason (for instance, one task
    is stuck), so we should not try to outsmart Roby and just kill
    the process.

--- lib/syskit/proxies.rb
+++ lib/syskit/proxies.rb
@@ -286,7 +286,7 @@ module Orocos
             # kill the deployment
             event :stop do |context|
 		ready_to_die!
-                orogen_deployment.kill(false)
+                orogen_deployment.kill(:wait => false, :kill_tasks => false)
             end
 
             # Creates a subclass of Deployment that represents the deployment
