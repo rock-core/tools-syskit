701e9881e16a65ae6d147e74d1dc996d2886ac12
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-07-14 17:12:16 +0200

    roby: in #merge_sort_order, look for data service proxies instead of abstract tasks
    The check on abstract tasks was meant to detect data service proxies.
    Since we have a much more robust way to do that now, use it.

--- lib/syskit/system.rb
+++ lib/syskit/system.rb
@@ -1347,7 +1347,7 @@ module Orocos
             def merge_sort_order(t1, t2)
                 MERGE_SORT_TRUTH_TABLE[ [!t1.finished?, !t2.finished?] ] ||
                     MERGE_SORT_TRUTH_TABLE[ [t1.execution_agent, t2.execution_agent] ] ||
-                    MERGE_SORT_TRUTH_TABLE[ [!t1.abstract?, !t2.abstract?] ] ||
+                    MERGE_SORT_TRUTH_TABLE[ [!t1.respond_to?(:proxied_data_services), !t2.respond_to?(:proxied_data_services)] ] ||
                     MERGE_SORT_TRUTH_TABLE[ [t1.fully_instanciated?, t2.fully_instanciated?] ] ||
                     MERGE_SORT_TRUTH_TABLE[ [t1.respond_to?(:__getobj__), t2.respond_to?(:__getobj__)] ]
             end
