3d0a21c5cda5d12a5e15239a8c77f30bca1ca975
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2010-11-01 12:23:47 +0100

    roby: forcefully specify the :output argument in deployment instances

--- lib/syskit/deployment.rb
+++ lib/syskit/deployment.rb
@@ -119,7 +119,7 @@ module Orocos
                 RobyPlugin.info { "starting deployment #{model.deployment_name} on #{host}" }
 
                 process_server, log_dir = Orocos::RobyPlugin.process_servers[host]
-                @orogen_deployment = process_server.start(model.deployment_name, :working_directory => log_dir)
+                @orogen_deployment = process_server.start(model.deployment_name, :working_directory => log_dir, :output => "%m-%p.txt")
                 Deployment.all_deployments[@orogen_deployment] = self
                 emit :start
             end
