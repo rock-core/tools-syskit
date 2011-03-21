5c1cd396ef8379464ce4efe0c51b324715772b9a
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-03-21 20:53:01 +0100

    roby: handle faceted selection / ambiguous service selection in scripts/instanciate_gui

--- lib/syskit/gui/instanciate_composition.rb
+++ lib/syskit/gui/instanciate_composition.rb
@@ -68,18 +68,25 @@ module Ui
                 model, actual_selection || self.actual_selection, name)
         end
 
+        def self.selection_to_string(selection)
+            case selection
+            when Orocos::RobyPlugin::ProvidedDataService
+                "#{selection.model.name}.#{selection.full_name}"
+            when Orocos::RobyPlugin::DeviceInstance
+                selection.name
+            when Class
+                selection.name
+            when String
+                selection
+            else
+                raise NotImplementedError
+            end
+        end
+
         def self.format_selection(result, actual_selection)
             actual_selection.each do |from, to|
-                if from.respond_to?(:to_str)
-                    from = "'#{from.to_str}'"
-                elsif from.respond_to?(:short_name)
-                    from = from.short_name
-                end
-                if to.respond_to?(:to_str)
-                    to = "'#{to.to_str}'"
-                elsif to.respond_to?(:short_name)
-                    to = to.short_name
-                end
+                from = selection_to_string(from)
+                to   = selection_to_string(to)
                 result << "\n  use(#{from} => #{to})"
             end
             result
@@ -151,89 +158,138 @@ module Ui
 
         attr_reader :main
 
+        module ComposerSvgItem
+            def selection_candidates_for(models)
+                # Get the task's role. We can safely assume the task
+                # has only one parent and is used for only one role
+                # in this parent
+                device_candidates = Hash.new
+                window.robot.devices.values.find_all do |dev|
+                    dev.service.fullfills?(models)
+                end.each do |dev|
+                    device_candidates[dev.name] = dev
+                end
+
+                if models.empty?
+                    return Hash.new, device_candidates
+                end
+
+                candidates = models.map do |m|
+                    window.engine.service_allocation_candidates[m] || ValueSet.new
+                end.map(&:to_value_set).inject(&:&)
+
+                # We now have to take care about ambiguity ... I.e.
+                # either use faceted selection or explicit service
+                # selection when needed
+                system_candidates = Hash.new
+                candidates.each do |model|
+                    disambiguation = []
+                    models.each do |expected_model|
+                        available_services =
+                            model.find_all_services_from_type(expected_model)
+                        if available_services.size == 1
+                            next
+                        else
+                            disambiguation << available_services
+                        end
+                    end
+
+                    # If there are multiple candidates, check if they have all a
+                    # different model. In this case, use the .as() mechanism.
+                    # Otherwise, we have to use the service name
+                    if disambiguation.size > 1
+                        raise NotImplementedError
+                    end
+                    disambiguation = disambiguation.first
+                    if !disambiguation
+                        system_candidates[model.short_name] = model
+                    elsif disambiguation.map(&:model).to_value_set == disambiguation.size
+                        disambiguation.each do |service|
+                            system_candidates["#{model.short_name}.as(#{service.model.short_name})"] = service
+                        end
+                    else
+                        disambiguation.each do |service|
+                            system_candidates[service.short_name] = service
+                        end
+
+                    end
+                end
+
+                return system_candidates, device_candidates
+            end
+
+            def mousePressEvent(event)
+                super
+
+                models =
+                    if task.respond_to?(:proxied_data_services)
+                        task.proxied_data_services
+                    else [task.model]
+                    end
+
+                system_candidates, device_candidates = selection_candidates_for(models)
+
+                roles = window.root_task.role_paths(task)
+                roles = roles.
+                    map { |role_path| role_path.join(".") }
+
+                current_selection = roles.find_all do |role_name|
+                    window.selection[role_name]
+                end
+
+                puts "mouse pressed for #{self} (#{models.map(&:name).join(", ")}) [#{task}, #{roles.to_a.join(", ")}]"
+                menu = Qt::Menu.new
+
+                device_candidates = device_candidates.to_a.sort_by(&:first)
+                system_candidates = system_candidates.to_a.sort_by(&:first)
+                if device_candidates.empty? && system_candidates.empty? && current_selection.empty?
+                    action = menu.add_action('No available selection')
+                    action.enabled = false
+                end
+
+                deselection = Hash.new
+                current_selection.each do |role_name|
+                    text = "Don't use for #{role_name}"
+                    deselection[text] = role_name
+                    menu.add_action(text)
+                end
+
+                selection = Hash.new
+                if !device_candidates.empty?
+                    menu.add_separator.text = "Devices"
+                    device_candidates.each do |name, m|
+                        selection[name] = m
+                        menu.add_action(name)
+                    end
+                end
+                if !system_candidates.empty?
+                    menu.add_separator.text = "Models"
+                    system_candidates.each do |name, m|
+                        selection[name] = m
+                        menu.add_action(name)
+                    end
+                end
+                return unless action = menu.exec(event.screenPos)
+
+                if selected_model = selection[action.text]
+                    roles.each do |child_name|
+                        puts "selected #{selected_model} for #{child_name}"
+                        window.selection[child_name] = selected_model
+                    end
+                elsif deselected_role = deselection[action.text]
+                    window.selection.delete(deselected_role)
+                end
+
+                window.update
+            end
+        end
+
+
         def display_svg(filename)
             items = super
             items.find_all { |it| it.respond_to?(:task) }.
                 each do |item|
-                    def item.mousePressEvent(event)
-                        super
-
-                        models =
-                            if task.respond_to?(:proxied_data_services)
-                                task.proxied_data_services
-                            else [task.model]
-                            end
-
-                        # Get the task's role. We can safely assume the task
-                        # has only one parent and is used for only one role
-                        # in this parent
-                        roles = window.root_task.role_paths(task).
-                            map { |role_path| role_path.join(".") }
-
-                        device_candidates =
-                            window.robot.devices.values.find_all do |dev|
-                                dev.service.fullfills?(models)
-                            end.to_value_set
-
-                        system_candidates = 
-                            if models.empty?
-                                ValueSet.new
-                            else
-                                models.map do |m|
-                                    window.engine.service_allocation_candidates[m] || ValueSet.new
-                                end.
-                                    map(&:to_value_set).
-                                    inject(&:&)
-                            end
-
-                        current_selection = roles.find_all do |role_name|
-                            window.selection[role_name]
-                        end
-
-                        puts "mouse pressed for #{self} (#{models.map(&:name).join(", ")}) [#{task}, #{roles.to_a.join(", ")}]"
-                        menu = Qt::Menu.new
-
-                        device_candidates = device_candidates.to_a.sort_by(&:short_name)
-                        system_candidates = system_candidates.to_a.sort_by(&:short_name)
-                        if device_candidates.empty? && system_candidates.empty? && current_selection.empty?
-                            action = menu.add_action('No available selection')
-                            action.enabled = false
-                        end
-
-                        deselection = Hash.new
-                        current_selection.each do |role_name|
-                            text = "Don't use for #{role_name}"
-                            deselection[text] = role_name
-                            menu.add_action(text)
-                        end
-
-                        selection = Hash.new
-                        if !device_candidates.empty?
-                            menu.add_separator.text = "Devices"
-                            device_candidates.each do |m|
-                                selection[m.name] = m.name
-                                action = menu.add_action(m.name)
-                            end
-                        end
-                        if !system_candidates.empty?
-                            menu.add_separator.text = "Models"
-                            system_candidates.each do |m|
-                                selection[m.name] = m
-                                action = menu.add_action(m.name)
-                            end
-                        end
-                        return unless action = menu.exec(event.screenPos)
-
-                        if selected_model = selection[action.text]
-                            roles.each do |child_name|
-                                window.selection[child_name] = selected_model
-                            end
-                        elsif deselected_role = deselection[action.text]
-                            window.selection.delete(deselected_role)
-                        end
-
-                        window.update
-                    end
+                    item.extend ComposerSvgItem
                 end
             items
         end
