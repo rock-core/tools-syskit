87a521aff2053cdf757b70622a6c5c1ec9c0402d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-05-18 19:47:48 +0200

    roby: add a shell interface for configuration updates
    One can now do
    
      orocos.reload_configuration
      orocos.redeploy
    
    To apply configuration changes. It may entail starting / stopping
    some components, though.

--- lib/syskit/shell.rb
+++ lib/syskit/shell.rb
@@ -14,6 +14,42 @@ module Orocos
 
             def dump_all_config(path, name = nil)
                 dump_task_config(Orocos::RobyPlugin::TaskContext, path, name)
+                nil
+            end
+
+            def mark_changed_configuration_as_not_reusable(changed)
+                Roby.execute do
+                    changed.each do |model_name, changed_sections|
+                        task_model = Roby.app.orocos_tasks[model_name]
+                        Roby.plan.find_tasks(task_model).each do |task|
+                            if task.conf.any? { |section_name| changed_sections.include?(section_name) }
+                                Robot.info "marking #{task} as not reusable as the configuration changed"
+                                task.do_not_reuse
+                                TaskContext.configured.delete(task.orocos_name)
+                            end
+                        end
+                    end
+                end
+            end
+
+            # Reloads the configuration files
+            def reload_config
+                if File.directory?(dir = File.join(APP_DIR, 'config', 'orogen'))
+                    changed = Orocos.conf.load_dir(dir)
+                    mark_changed_configuration_as_not_reusable(changed)
+                end
+                if Roby.app.robot_name && File.directory?(dir = File.join(APP_DIR, 'config', Roby.app.robot_name, 'orogen'))
+                    changed = Orocos.conf.load_dir(dir)
+                    mark_changed_configuration_as_not_reusable(changed)
+                end
+                nil
+            end
+
+            # Require the engine to redeploy the current network. Useful to
+            # apply changed configuration files
+            def redeploy
+                Roby.app.orocos_engine.modified!
+                nil
             end
         end
     end
