9e128c0bf69b19ab0ca159e7706a4bd90c3086fe
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2011-06-10 19:53:07 +0200

    roby: call #cleanup when quitting the deployments

--- lib/syskit/deployment.rb
+++ lib/syskit/deployment.rb
@@ -285,6 +285,13 @@ module Orocos
             # Stops all tasks that are running on top of this deployment, and
             # kill the deployment
             event :stop do |context|
+                if task_handles
+                    task_handles.each_value do |t|
+                        if t.rtt_state != :PRE_OPERATIONAL
+                            t.cleanup
+                        end
+                    end
+                end
 		ready_to_die!
                 orogen_deployment.kill(false)
             end
