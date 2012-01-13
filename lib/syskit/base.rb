c6533eccc54372b7f37c4eedf4a8ee5ef1815cd7
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-01-13 13:54:13 +0100

    roby: make dummy orogen interfaces truly anonymous
    The oroGen interfaces we create in the rock-roby code do not
    need a name, so don't provide one.

--- lib/syskit/base.rb
+++ lib/syskit/base.rb
@@ -32,16 +32,7 @@ module Orocos
 
         # Creates a blank orogen interface and returns it
         def self.create_orogen_interface(name)
-            basename = "roby_#{name.gsub(/[^\w]/, '_')}".camelcase(:upper)
-	    begin
-		Roby.app.main_orogen_project.find_task_context(basename)
-                basename << "_DD"
-            rescue ArgumentError
-            end
-
-            interface = Roby.app.main_orogen_project.
-                external_task_context(basename)
-            interface
+            Orocos::Spec::TaskContext.new(Roby.app.main_orogen_project)
         end
 
         # Returns an array of modules. It is used as the search path for DSL
