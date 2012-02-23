a891d7760bc55ca975ca7809462f77791a772f13
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2012-02-23 18:09:30 +0100

    roby: allow accessing the functionality of Roby's scripts/run through rock-roby run

--- lib/syskit/scripts/run.rb
+++ lib/syskit/scripts/run.rb
@@ -4,15 +4,38 @@ require 'roby/schedulers/temporal'
 Scripts = Orocos::RobyPlugin::Scripts
 
 dry_run = false
+run_roby = false
 parser = OptionParser.new do |opt|
-    opt.banner = "Usage: scripts/orocos/run [options] deployment [additional_things_to_run]\nwhere 'deployment' is either the name of a deployment in config/deployments,\nor a file that should be loaded to get the desired deployment"
+    opt.banner =
+"usage: run [options] deployment [additional_things_to_run]
+        run -r robot_name[:robot_type] -c
+
+In the first form, load a deployment file and run it, optionally adding some
+more things to the running system (such as devices or definitions). If no
+deployment file needs to be loaded (e.g. to run a device), use '-' as the
+deployment name. A -rrobot_name option can be given to configure as a specific
+robot
+
+In the second form, run the specified Roby controller"
     opt.on('--dry-run', "do not configure and start any module") do
         dry_run = true
+    end
+    opt.on('-c', 'run the Roby controller for the specified robot') do
+        run_roby = true
     end
 end
 
 Scripts.common_options(parser, false)
 remaining = parser.parse(ARGV)
+
+if run_roby
+    ARGV.clear
+    ARGV << Roby.app.robot_name
+    ARGV << Roby.app.robot_type
+    require 'roby/app/scripts/run'
+    exit 0
+end
+
 if remaining.empty?
     STDERR.puts parser
     exit(1)
