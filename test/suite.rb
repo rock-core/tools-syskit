f3e5a14ae5a8cb4c819a745931aeecb4280dbfd6
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2012-12-12 15:17:10 +0100

    create a separate suite_models.
    It is still required by suite.rb, but allows to run all model tests
    separately

--- test/suite.rb
+++ test/suite.rb
@@ -3,15 +3,7 @@ if !ENV['SYSKIT_ENABLE_COVERAGE']
 end
 require 'syskit/test'
 
-require './test/models/test_base'
-require './test/models/test_bound_data_service'
-require './test/models/test_component'
-require './test/models/test_composition'
-require './test/models/test_composition_specialization'
-require './test/models/test_data_services'
-require './test/models/test_deployment'
-require './test/models/test_specialization_manager'
-require './test/models/test_task_context'
+require './test/suite_models'
 
 require './test/test_abstract_placeholders'
 require './test/test_bound_data_service'
---
+++ test/suite_models.rb
@@ -0,0 +1,18 @@
+if !ENV['SYSKIT_ENABLE_COVERAGE']
+    ENV['SYSKIT_ENABLE_COVERAGE'] = '1'
+end
+require 'syskit/test'
+
+require './test/models/test_base'
+require './test/models/test_port'
+require './test/models/test_bound_data_service'
+require './test/models/test_component'
+require './test/models/test_composition'
+require './test/models/test_composition_specialization'
+require './test/models/test_data_services'
+require './test/models/test_deployment'
+require './test/models/test_specialization_manager'
+require './test/models/test_task_context'
+
+Syskit.logger = Logger.new(File.open("/dev/null", 'w'))
+Syskit.logger.level = Logger::DEBUG
