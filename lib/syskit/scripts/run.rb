43c3e7501e6b63e47fed23768faff064b6291776
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-02-01 16:10:42 +0100

    scripts: adapt the run script to the usage of actions instead of definitions

--- lib/syskit/scripts/run.rb
+++ lib/syskit/scripts/run.rb
@@ -7,21 +7,14 @@ dry_run = false
 run_roby = false
 parser = OptionParser.new do |opt|
     opt.banner =
-"usage: run [options] deployment [additional_things_to_run]
-        run -r robot_name[:robot_type] -c
-
-In the first form, load a deployment file and run it, optionally adding some
-more things to the running system (such as devices or definitions). If no
-deployment file needs to be loaded (e.g. to run a device), use '-' as the
-deployment name. A -rrobot_name option can be given to configure as a specific
-robot
-
-In the second form, run the specified Roby controller"
-    opt.on('--dry-run', "do not configure and start any module") do
-        dry_run = true
-    end
+"usage: run [options] -r robot_name [actions_to_run]
+        run -r robot_name[:robot_type] -c"
     opt.on('-c', 'run the Roby controller for the specified robot') do
         run_roby = true
+    end
+    opt.on('-h', '--help', 'show this help message') do
+        puts parser
+        exit(0)
     end
 end
 
@@ -36,13 +29,6 @@ if run_roby
     exit 0
 end
 
-if remaining.empty?
-    STDERR.puts parser
-    exit(1)
-end
-deployment_file     = remaining.shift
-additional_services = remaining.dup
-
 Roby.app.public_shell_interface = true
 Roby.app.public_logs = true
 
@@ -51,22 +37,8 @@ error = Scripts.run do
     Roby.app.run do
         Scripts.toc_tic "fully initialized in %.3f seconds"
         Roby.execute do
-            if deployment_file != '-'
-                Roby.app.load_orocos_deployment(deployment_file)
-            end
-            additional_services.each do |service_name|
-                Scripts.add_service(service_name)
-            end
-            Roby.app.syskit_engine.resolve
-            if !Roby.engine.scheduler
-                require 'roby/schedulers/temporal'
-                Roby.engine.scheduler = Roby::Schedulers::Temporal.new
-            end
-
-            tasks = Roby.plan.find_tasks(Syskit::Component).
-                roots(Roby::TaskStructure::Hierarchy).to_value_set
-            tasks.each do |t|
-                Roby.plan.add_mission(t)
+            remaining.each do |action_name|
+                Robot.send("#{action_name}!")
             end
         end
     end
