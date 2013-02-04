9ded03d3e6ef4ef6907eec985426d11610f67d82
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-02-04 12:01:39 +0100

    app: delete the robot extensions copied from orocos.rb
    They are completely obsolete now ...

--- lib/syskit/roby_app/robot_extension.rb
+++ lib/syskit/roby_app/robot_extension.rb
@@ -2,17 +2,6 @@ module Syskit
     module RobyApp
         # Extensions to Roby's main Robot object
         module RobotExtension
-            def each_device(&block)
-                Roby.app.syskit_engine.robot.devices.each_value(&block)
-            end
-
-            def devices(&block)
-                if block
-                    Roby.app.syskit_engine.robot.instance_eval(&block)
-                else
-                    each_device
-                end
-            end
         end
     end
 end
