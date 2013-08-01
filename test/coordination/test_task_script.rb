2027dbbfecb487208841673600088f569a7edf42
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-08-01 10:58:26 +0200

    test for task scripts at model level

--- test/coordination/test_task_script.rb
+++ test/coordination/test_task_script.rb
@@ -11,6 +11,81 @@ describe Syskit::Coordination::TaskScriptExtension do
         assert_equal composition_m.test_child, composition_m.script.test_child.model.model
     end
 
+    describe "model-level scripts" do
+        attr_reader :base_srv_m, :srv_m, :component_m, :composition_m
+        before do
+            @base_srv_m = Syskit::DataService.new_submodel do
+                input_port 'base_in', '/int'
+                output_port 'base_out', '/int'
+            end
+            @srv_m = Syskit::DataService.new_submodel do
+                input_port 'srv_in', '/int'
+                output_port 'srv_out', '/int'
+            end
+            srv_m.provides base_srv_m, 'base_in' => 'srv_in', 'base_out' => 'srv_out'
+            @component_m = stub_syskit_task_context_model 'Task' do
+                input_port 'in', '/int'
+                output_port 'out', '/int'
+            end
+            component_m.provides srv_m, :as => 'test'
+            @composition_m = Syskit::Composition.new_submodel
+            composition_m.add base_srv_m, :as => 'test'
+        end
+
+        describe "when mapping ports from services" do
+            def start
+                component = syskit_deploy_task_context(component_m)
+                composition = composition_m.use('test' => component).instanciate(plan)
+                plan.add_permanent(composition)
+                syskit_start_component(composition)
+                syskit_start_component(component)
+                return composition, component
+            end
+
+            it "gives writer access to input ports mapped from services" do
+                writer = nil
+                composition_m.script do
+                    writer = test_child.base_in_port.writer
+                end
+                composition, component = start
+                writer.write(10)
+                assert_equal 10, composition.test_child.orocos_task.in.read
+            end
+
+            it "gives reader access to input ports mapped from services" do
+                reader = nil
+                composition_m.script do
+                    reader = test_child.base_out_port.reader
+                end
+                composition, component = start
+                composition.test_child.orocos_task.out.write(10)
+                assert_equal 10, reader.read
+            end
+        end
+
+        it "gives writer access to input ports" do
+            writer = nil
+            component_m.script do
+                writer = in_port.writer
+            end
+            component = syskit_deploy_task_context(component_m)
+            syskit_start_component(component)
+            writer.write(10)
+            assert_equal 10, component.orocos_task.in.read
+        end
+
+        it "gives access to output ports" do
+            reader = nil
+            component_m.script do
+                reader = out_port.reader
+            end
+            component = syskit_deploy_task_context(component_m)
+            syskit_start_component(component)
+            component.orocos_task.out.write(10)
+            assert_equal 10, reader.read
+        end
+    end
+
     describe "input port access" do
         attr_reader :component, :srv_m
 
@@ -27,7 +102,7 @@ describe Syskit::Coordination::TaskScriptExtension do
             assert_kind_of Syskit::InputPort, port
         end
 
-        it "gives access to input ports" do
+        it "gives access to input ports when created at the instance level" do
             writer = nil
             component.script do
                 writer = in_port.writer
