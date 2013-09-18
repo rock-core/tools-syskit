2613ff3093cec9ae608613fc9aa7b21726da2347
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-09-18 18:30:38 +0200

    test: pretty-print the rescued exceptions

--- lib/syskit/test/spec.rb
+++ lib/syskit/test/spec.rb
@@ -5,6 +5,19 @@ module Syskit
             include Test::ActionAssertions
             include Test::NetworkManipulation
 
+            def self.it(*args, &block)
+                super(*args) do
+                    begin
+                        instance_eval(&block)
+                    rescue Exception => e
+                        if e.class.name =~ /Syskit|Roby/
+                            pp e
+                        end
+                        raise
+                    end
+                end
+            end
+
             def plan; Roby.plan end
         end
     end
