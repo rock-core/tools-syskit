b2ab2d7fc08dc8bf477cd911299a4675a984505e
Thomas Roehr <thomas.roehr@dfki.de>
2013-10-23 12:21:21 +0200

    ros: cleanup Syskit::ROS::Node

--- lib/syskit/ros/node.rb
+++ lib/syskit/ros/node.rb
@@ -5,31 +5,10 @@ module Syskit
             include Logger::Hierarchy
 
             def initialize(arguments = Hash.new)
-                options, task_options = Kernel.filter_options arguments,
-                    :orogen_model => nil
-                super(task_options)
-
-                # While the spec can be the task deployment, the orogen model
-                # will be Orocos::ROS::Spec::Node
-                @orogen_model   = options[:orogen_model] ||
-                    Orocos::Spec::TaskDeployment.new(nil, model.orogen_model)
-
-                # All tasks start with executable? and setup? set to false
-                #
-                # Then, the engine will call setup, which will do what it should
-                @setup = false
-                self.executable = false
+                super
             end
 
-            def can_merge?(other_task)
-                NetworkGeneration.debug { "cannot merge #{other_task} in #{self}: #{self} does not support merging" }
-                # ROS Task cannot be merged
-                false
-            end
 
-            def merge(merged_task)
-                raise RuntimeError, "#{self} does not support merging of task"
-            end
 
             def needs_reconfiguration?
                 false
@@ -60,7 +39,7 @@ module Syskit
                 if Orocos::ROS.rosnode_running?(orogen_model.ros_name)
                     is_setup!
                 else
-                    raise InternalError, "#setup called but ROS node '#{name}' is not running -- note that currently, ROS nodes have to permanently running to be used with syskit"
+                    raise InternalError, "#setup called but ROS node '#{name}' is not running"
                 end
             end
 
