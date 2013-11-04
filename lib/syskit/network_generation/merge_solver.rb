655892332f21c5ffd1fc128f4fac75b7ec37f8a6
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-11-04 20:08:42 +0100

    netgen: when filtering the possible cycles, take into account that replacement mignht map two task pairs onto the same pair
    This triggered a bug when we add the task pair into the merge graph.
    Since we were getting twice the same pair, the edge already existed
    and #link raised.

--- lib/syskit/network_generation/merge_solver.rb
+++ lib/syskit/network_generation/merge_solver.rb
@@ -655,6 +655,8 @@ module Syskit
                 # Filter.
                 possible_cycles = possible_cycles.map do |task, target_task|
                     task, target_task = replacement_for(task), replacement_for(target_task)
+		    next if merge_graph.linked?(task, target_task)
+
                     result = resolve_single_merge(task, target_task)
                     if result.nil?
                         [task, target_task]
