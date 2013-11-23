8738b9c20186559292cb2dd74006b2352fb30b86
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-11-23 09:54:43 +0100

    app: the ROS deployment loading methods now simply need to call the base ones with a ROS process server

--- lib/syskit/ros/roby_app/configuration.rb
+++ lib/syskit/ros/roby_app/configuration.rb
@@ -6,16 +6,8 @@ module Syskit
             #
             # @return the ROS::Nodes in use
             def use_ros_launchers_from(project_name, options = Hash.new)
-                project = Roby.app.using_ros_package(project_name)
-                if project.ros_launchers.empty?
-                    raise ArgumentError, "Syskit::ROS: did not find any launchers for project: '#{project_name}'. Search dirs: #{Orocos::ROS.spec_search_directories.join(",")}"
-                end
-
-                project.ros_launchers.each do |launcher|
-                    use_ros_launcher(launcher.name, options)
-                end
+                use_deployments_from(project_name, Hash[:on => 'ros'].merge(options))
             end
-
 
             # Add the given launcher (referred to by its process name, that is
             # the name given in the oroGen file) to the set of deployments the
@@ -27,29 +19,8 @@ module Syskit
                 if !names.last.kind_of?(Hash)
                     names << Hash.new
                 end
-
-                new_launchers, options = Orocos::ROS::LauncherProcess.parse_run_options(*names)
-
-                options, run_options = Kernel.validate_options names.last, :on => 'ros'
-
-                new_launchers.each do |launcher_name|
-                    model = app.using_ros_launcher(launcher_name, options)
-
-                    configured_deployment = Models::ConfiguredDeployment.new(options[:on], model)
-
-                    configured_deployment.each_orogen_deployed_task_context_model do |task|
-                        orocos_name = task.name
-                        if deployed_tasks[orocos_name] && deployed_tasks[orocos_name] != configured_deployment
-                            raise TaskNameAlreadyInUse.new(orocos_name, deployed_tasks[orocos_name], configured_deployment), "there is already a deployment that provides #{orocos_name}"
-                        end
-                    end
-                    configured_deployment.each_orogen_deployed_task_context_model do |task|
-                        deployed_tasks[task.name] = configured_deployment
-                    end
-                    deployments[options[:on]] << configured_deployment
-                    configured_deployment
-                end
-                model
+                names[-1] = Hash[:on => 'ros'].merge(names[-1])
+                use_deployment(*names)
             end
         end
     end
