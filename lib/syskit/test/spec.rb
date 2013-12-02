2aef1905eb67e98efdfafedb3413a08bec35e6e2
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-12-02 09:00:15 +0100

    test: create the stub_syskit_attached_device helper
    This is used for testing of com bus drivers

--- lib/syskit/test/spec.rb
+++ lib/syskit/test/spec.rb
@@ -18,6 +18,32 @@ module Syskit
                 end
             end
 
+            # Create a stub device task attached to a given communication bus
+            #
+            # This is meant to be used to test the integration of specific com
+            # busses. The driver task from the communication bus is expected to
+            # exist, while the driver task for the device is stubbed as well.
+            #
+            # The bus is called 'bus'
+            #
+            # @param [String] dev_name the name of the created device
+            # @return [Syskit::Component] the device driver task
+            def stub_syskit_attached_device(bus_m, dev_name = 'dev')
+                robot = Syskit::Robot::RobotDefinition.new
+                dev_m = Syskit::Device.new_submodel(:name => "StubDevice")
+                driver_m = Syskit::TaskContext.new_submodel(:name => "StubDriver") do
+                    input_port 'bus_in', bus_m.message_type
+                    output_port 'bus_out', bus_m.message_type
+                    provides bus_m.client_srv, :as => 'can'
+                    driver_for dev_m, :as => 'dev'
+                end
+                stub_syskit_deployment_model(driver_m, 'driver_task')
+                bus = robot.com_bus bus_m, :as => 'bus'
+                stub_syskit_deployment_model(bus.driver_model, 'bus_task')
+                robot.device(dev_m, :as => dev_name).
+                    attach_to(bus)
+            end
+
             def plan; Roby.plan end
         end
     end
