3c54d0003eecf65a57ad2067156c4b9079e4a8d4
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-12-03 13:51:27 +0100

    test: skip tests that don't have the proper typekit/task library installed

--- lib/syskit/test/component_test.rb
+++ lib/syskit/test/component_test.rb
@@ -2,6 +2,25 @@ module Syskit
     module Test
         class ComponentTest < Spec
             include Syskit::Test
+
+            def self.roby_should_run(test, app)
+                super
+
+                if app.simulation?
+                    begin
+                        ruby_task = Orocos::RubyTasks::TaskContext.new(
+                            "spec#{object_id}", :model => desc.orogen_model)
+                        ruby_task.dispose
+                    rescue ::Exception => e
+                        test.skip("#{test.__name__} cannot run: #{e.message}")
+                    end
+                else
+                    project_name = desc.orogen_model.project.name
+                    if !Orocos.default_pkgconfig_loader.has_project?(project_name)
+                        test.skip("#{test.__name__} cannot run: the task is not available")
+                    end
+                end
+            end
         end
     end
 end
