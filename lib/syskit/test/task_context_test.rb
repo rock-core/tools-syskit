6b6f0e49eacd0507dc69a41b6067a5eee3c3184a
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-12-10 22:59:56 +0100

    test: fix having a describe block with a string within a subclass of TaskContextTest

--- lib/syskit/test/task_context_test.rb
+++ lib/syskit/test/task_context_test.rb
@@ -1,6 +1,14 @@
 module Syskit
     module Test
         class TaskContextTest < ComponentTest
+            def self.subject_syskit_model
+                current = self
+                while current && !current.desc.respond_to?(:orogen_model)
+                    current = current.superclass
+                end
+                current.desc
+            end
+
             # Helper that creates a standard test which configures the
             # underlying task
             def self.it_should_be_configurable
@@ -24,13 +32,13 @@ module Syskit
                 if app.simulation?
                     begin
                         ruby_task = Orocos::RubyTasks::TaskContext.new(
-                            "spec#{object_id}", :model => desc.orogen_model)
+                            "spec#{object_id}", :model => subject_syskit_model.orogen_model)
                         ruby_task.dispose
                     rescue ::Exception => e
                         test.skip("#{test.__name__} cannot run: #{e.message}")
                     end
                 else
-                    project_name = desc.orogen_model.project.name
+                    project_name = subject_syskit_model.orogen_model.project.name
                     if !Orocos.default_pkgconfig_loader.has_project?(project_name)
                         test.skip("#{test.__name__} cannot run: the task is not available")
                     end
