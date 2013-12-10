5ccbbcfc3b8f6a009c3c01b0b8035f5127445ae2
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-12-10 20:01:19 +0100

    test: split test helpers into a ComponentTest class and a TaskContextTest class
    The existing code was really TaskContext-specific, which made
    testing anything else (i.e. a composition) impossible.

---
+++ lib/syskit/test/task_context_test.rb
@@ -0,0 +1,42 @@
+module Syskit
+    module Test
+        class TaskContextTest < ComponentTest
+            # Helper that creates a standard test which configures the
+            # underlying task
+            def self.it_should_be_configurable
+                it "should be configurable" do
+                    component_m = self.class.desc
+                    if driver_srv = component_m.each_master_driver_service.first
+                        component_m = stub_syskit_driver(driver_srv.model,
+                                                         :as => 'driver_task',
+                                                         :using => component_m)
+                    else
+                        stub_syskit_deployment_model(component_m, 'task')
+                    end
+                    task = syskit_run_deployer(component_m)
+                    syskit_setup_component(task)
+                end
+            end
+
+            def self.roby_should_run(test, app)
+                super
+
+                if app.simulation?
+                    begin
+                        ruby_task = Orocos::RubyTasks::TaskContext.new(
+                            "spec#{object_id}", :model => desc.orogen_model)
+                        ruby_task.dispose
+                    rescue ::Exception => e
+                        test.skip("#{test.__name__} cannot run: #{e.message}")
+                    end
+                else
+                    project_name = desc.orogen_model.project.name
+                    if !Orocos.default_pkgconfig_loader.has_project?(project_name)
+                        test.skip("#{test.__name__} cannot run: the task is not available")
+                    end
+                end
+            end
+        end
+    end
+end
+
