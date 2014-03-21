34e39161678440e11ba7fd185b71242946f5ea3f
Malte Wirkus <malte.wirkus@dfki.de>
2014-03-21 09:12:54 +0100

    added timeout_poll to script extension

--- lib/syskit/coordination/task_script_extension.rb
+++ lib/syskit/coordination/task_script_extension.rb
@@ -1,6 +1,7 @@
 module Syskit
     module Coordination
         module TaskScriptExtension
+        
 	    # Waits until this data writer is {InputWriter#ready?}
         def wait_until_ready(writer)
             poll do
@@ -26,6 +27,34 @@ module Syskit
                 current_time = Types::Base::Time.now
                 if(current_time - start_time >= delay)
                     transition!
+                end
+            end
+        end
+
+        class Timeout < RuntimeError
+        end
+                
+        def timeout_poll(timeout, &block)
+            if not timeout
+                puts "Will wait forever..."
+            end
+            
+            start_time = Types::Base::Time.now
+
+            poll do
+                current_time = Types::Base::Time.now
+                waited = current_time - start_time
+                if(timeout and waited > timeout)
+                    raise Timeout, "Timeout after #{waited} seconds. Configred: #{timeout}"
+                end
+                
+                begin
+                    puts "Calling block..."
+                    block.call
+                rescue Exception => e
+                    puts "Exception in user provided block"
+                    puts e
+                    raise e
                 end
             end
         end
