3e6c82e51370dce87fd2efde39d9400a4acd66ee
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-07-05 15:03:43 +0200

    test: no need to call Minitest.autorun explicitely anymore

--- test/suite.rb
+++ test/suite.rb
@@ -30,8 +30,6 @@ require './test/test_typelib_marshalling'
 
 require './test/test_exceptions'
 
-MiniTest::Unit.autorun
-
 Syskit.logger = Logger.new(File.open("/dev/null", 'w'))
 Syskit.logger.level = Logger::DEBUG
 
