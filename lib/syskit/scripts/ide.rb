6c791912ac1087e91e12fef621c9c968f83bea1e
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-04 17:20:55 -0300

    gui: start improving the model browser, and add the 'syskit ide' subcommand
    syskit ide will in term replace syskit browse, but not quite yet.
    
    For now, this is mostly like syskit browse, but with bugfixes, and with
    the ability to open files in an external editor (needs some configuration)

--- lib/syskit/scripts/ide.rb
+++ lib/syskit/scripts/ide.rb
@@ -1,14 +1,11 @@
+require 'syskit/gui/ide'
 require 'roby/standalone'
 require 'syskit/scripts/common'
-require 'Qt'
-require 'syskit/gui/browse'
-
-Scripts = Syskit::Scripts
 
 load_all = false
 parser = OptionParser.new do |opt|
     opt.banner = <<-EOD
-Usage: browse [file] [options]
+Usage: ide [file] [options]
 Loads the models from this bundle and allows to browse them. If a file is given, only this file is loaded.
     EOD
 
@@ -16,7 +13,7 @@ Loads the models from this bundle and allows to browse them. If a file is given,
         load_all = true
     end
 end
-Scripts.common_options(parser, true)
+Syskit::Scripts.common_options(parser, true)
 remaining = parser.parse(ARGV)
 
 # We don't need the process server, win some startup time
@@ -34,30 +31,16 @@ Roby.app.auto_load_models = direct_files.empty?
 Roby.app.additional_model_files.concat(direct_files)
 
 app = Qt::Application.new(ARGV)
-settings = Qt::Settings.new('syskit', '')
 
-Scripts.run do
+Syskit::Scripts.run do
     Roby.app.syskit_engine.prepare
-    main = Syskit::GUI::Browse.new
+    main = Syskit::GUI::IDE.new
 
-    # Select the model given on the command line (if any)
-    if !model_names.empty?
-        model = begin
-                    constant(model_names.first)
-                rescue NameError
-                    Syskit.warn "cannot find a model named #{remaining.first}"
-                end
-        if model
-            main.select_by_module(model)
-        end
-    end
-
-    size = settings.value("MainWindow/size", Qt::Variant.new(Qt::Size.new(800, 600))).to_size
-    main.resize(size)
+    main.restore_from_settings
     main.show
     $qApp.exec
-    settings.setValue('MainWindow/size', Qt::Variant.new(main.size))
-    settings.sync
+    main.save_to_settings
+    main.settings.sync
 end
 
 
