3e4f9516b1be39c2abec899c05f21a4874847ab7
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-04 15:53:40 -0300

    doc: tune the yard plugin some more
    It now properly declares the OroGen and OroGen::TaskLibrary modules
    so that the hierarchy renders properly in the generated index
    
    See https://github.com/lsegal/yard/issues/892

--- lib/yard-syskit.rb
+++ lib/yard-syskit.rb
@@ -1,23 +1,7 @@
-module Roby
+require 'facets/string/camelcase'
+module Syskit
     module YARD
         include ::YARD
-
-        class OroGenHandler < YARD::Handlers::Ruby::ClassHandler
-            def self.handles?(node)
-                return if !super
-                node.class_name.namespace[0] == "OroGen"
-            end
-
-            def parse_superclass(statement)
-                # We assume that all classes in OroGen have Syskit::TaskContext
-                # as superclass by default
-                if !statement
-                    statement = ::YARD.parse_string("Syskit::TaskContext").
-                        enumerator.first
-                end
-                super(statement)
-            end
-        end
 
         class DataServiceProvidesHandler < ::YARD::Handlers::Ruby::MixinHandler
             handles method_call(:provides)
@@ -28,7 +12,7 @@ module Roby
             end
         end
 
-        class DataServiceBaseDSL < YARD::Handlers::Ruby::Base
+        class DSLBaseHandler < YARD::Handlers::Ruby::Base
             namespace_only
 
             def process
@@ -61,20 +45,58 @@ module Roby
             end
         end
 
-        class DataServiceTypeDSL < DataServiceBaseDSL
+        class DataServiceTypeDSL < DSLBaseHandler
             handles method_call(:data_service_type)
         end
 
-        class DeviceTypeDSL < DataServiceBaseDSL
+        class DeviceTypeDSL < DSLBaseHandler
             handles method_call(:device_type)
         end
 
-        class ComBusTypeDSL < DataServiceBaseDSL
+        class ComBusTypeDSL < DSLBaseHandler
             handles method_call(:com_bus_type)
         end
 
-        class ProfileDSL < DataServiceBaseDSL
+        class ProfileDSL < DSLBaseHandler
             handles method_call(:profile)
+        end
+
+        class UsingTaskLibrary < YARD::Handlers::Ruby::Base
+            handles method_call(:using_task_library)
+
+            def process
+                orogen_m = ModuleObject.new(namespace, '::OroGen')
+                project_m = ModuleObject.new(orogen_m, call_params[0].camelcase(:upper))
+                register project_m
+                project_m.docstring.replace("Created by Syskit to represent the #{call_params[0]} oroGen project")
+            end
+        end
+
+        class OroGenHandler < YARD::Handlers::Ruby::ClassHandler
+            handles :class
+            namespace_only
+
+            def self.handles?(node)
+                return if !super
+                node.class_name.namespace[0] == "OroGen"
+            end
+
+            def process
+                path = statement.class_name.source.split('::')
+                orogen_m = ModuleObject.new(namespace, '::OroGen')
+                ModuleObject.new(orogen_m, path[1])
+                super
+            end
+
+            def parse_superclass(statement)
+                # We assume that all classes in OroGen have Syskit::TaskContext
+                # as superclass by default
+                if !statement
+                    statement = ::YARD.parse_string("Syskit::TaskContext").
+                        enumerator.first
+                end
+                super(statement)
+            end
         end
     end
 end
