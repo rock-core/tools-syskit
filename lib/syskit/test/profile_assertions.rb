a19683d98cb030706aaae6246c8817ab20289ac3
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-08-18 13:41:57 -0300

    test: fix profile tests not handling the profile devices

--- lib/syskit/test/profile_assertions.rb
+++ lib/syskit/test/profile_assertions.rb
@@ -16,9 +16,7 @@ module Syskit
             # @raise [ArgumentError] if the argument is invalid
             def Actions(arg)
                 if arg.kind_of?(Syskit::Actions::Profile)
-                    arg.definitions.each_key.map do |name|
-                        arg.resolved_definition(name)
-                    end
+                    arg.each_action
                 elsif arg.respond_to?(:to_action)
                     [arg]
                 elsif arg.respond_to?(:flat_map)
