ea9d0a9513ddd1641db7919ccfc080c177a175ad
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-10-30 17:44:08 -0200

    minor cleanup

--- lib/syskit/runtime/exceptions.rb
+++ lib/syskit/runtime/exceptions.rb
@@ -16,13 +16,8 @@ module Syskit
                     pp.text "the RTT task #{failed_task.orocos_name} of type #{failed_task.model.short_name} was expected to have a port called #{port_name}, but does not."
                     pp.breakable
 
-                    if failed_task.model.private_specialization?
-                        dynamic_model, static_model =
-                            failed_task.model, failed_task.model.superclass
-                    else
-                        dynamic_model, static_model =
-                            nil, failed_task.model
-                    end
+                    dynamic_model, static_model =
+                        failed_task.model, failed_task.concrete_model
 
                     # Might have been a wrong configure() implementation. Check.
                     if dynamic_model &&
@@ -30,17 +25,8 @@ module Syskit
                         !static_model.send("find_#{port_kind}_port", port_name)
 
                         pp.text "it is a dynamic port that should have been created by #{static_model.short_name}#configure"
-                    end
-
-                    if static_model.send("find_#{port_kind}_port", port_name)
+                    elsif static_model.send("find_#{port_kind}_port", port_name)
                         pp.text "it is a static port that should be there on every task of type #{static_model.short_name}"
-                    end
-
-                    # Check for com bus (they do not use dynamic slaves yet)
-                    if failed_task.respond_to?(:port_to_device) &&
-                        (device_name = failed_task.port_to_device[port_name])
-
-                        pp.text "it is a dynamic port that should have been created by #{static_model.short_name}#configure to accomodate the device(s) #{device_name.join(", ")}"
                     end
                 end
             end
