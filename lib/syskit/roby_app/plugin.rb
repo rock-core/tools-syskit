bf8f68ed1b2eaa899c0fcbf852839d8fc6511bd7
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-06-06 16:14:40 -0300

    app: register the deployer models at project-loading time
    This ensures that one can use the loader APIs and see all loaded models
    mapped automatically into Syskit.

--- lib/syskit/roby_app/plugin.rb
+++ lib/syskit/roby_app/plugin.rb
@@ -266,6 +266,9 @@ module Syskit
                     end
                 end
 
+                orogen.each_deployment do |deployer_model|
+                    deployment_define_from_orogen(deployer_model)
+                end
                 orogen
             end
 
@@ -592,10 +595,11 @@ module Syskit
 
             # Loads the oroGen deployment model for the given name and returns
             # the corresponding syskit model
-            def using_deployment(name, options = Hash.new)
-                options = Kernel.validate_options options, :loader => default_loader
-                deployer = options[:loader].deployment_model_from_name(name)
-                deployment_define_from_orogen(deployer)
+            def using_deployment(name, loader: default_loader)
+                # This loads the underlying orogen project which causes the
+                # deployer to be registered
+                deployment_model = loader.deployment_model_from_name(name)
+                Deployment.find_model_by_orogen(deployment_model)
             end
 
             # Loads the oroGen deployment model based on a ROS launcher file
