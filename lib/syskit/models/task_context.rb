eb6980f78ab5c0acce0183144e4934de56528a04
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-12-19 15:38:34 -0200

    replace some hash notations by keyword arguments

--- lib/syskit/models/task_context.rb
+++ lib/syskit/models/task_context.rb
@@ -82,9 +82,7 @@ module Syskit
             # Creates a subclass of TaskContext that represents the given task
             # specification. The class is registered as
             # Roby::Orogen::ProjectName::ClassName.
-            def define_from_orogen(orogen_model, options = Hash.new)
-                options = Kernel.validate_options options,
-                    :register => false
+            def define_from_orogen(orogen_model, register: false)
                 if model = find_model_by_orogen(orogen_model) # already defined, probably because of dependencies
                     return model
                 end
@@ -94,11 +92,11 @@ module Syskit
                     supermodel = self
                 else
                     supermodel = find_model_by_orogen(superclass) ||
-                        define_from_orogen(superclass, :register => options[:register])
+                        define_from_orogen(superclass, register: register)
                 end
-                klass = supermodel.new_submodel(:orogen_model => orogen_model)
+                klass = supermodel.new_submodel(orogen_model: orogen_model)
 
-                if options[:register] && orogen_model.name
+                if register && orogen_model.name
                     register_syskit_model_from_orogen_name(klass)
                 end
 
@@ -161,7 +159,7 @@ module Syskit
             # A state_name => event_name mapping that maps the component's
             # state names to the event names that should be emitted when it
             # enters a new state.
-            inherited_attribute(:state_event, :state_events, :map => true) { Hash.new }
+            inherited_attribute(:state_event, :state_events, map: true) { Hash.new }
 
             # Create a new TaskContext model
             #
@@ -184,15 +182,13 @@ module Syskit
             #
             # @param [String] name an optional name for this submodel
             # @return [void]
-            def setup_submodel(submodel, options = Hash.new)
-                orogen_model, options = Kernel.filter_options options, :orogen_model
-                if !(m = orogen_model[:orogen_model])
-                    m = self.orogen_model.class.new(Roby.app.default_orogen_project, nil)
-                    m.extended_state_support
-                    m.subclasses self.orogen_model
+            def setup_submodel(submodel, orogen_model: nil, **options)
+                if !orogen_model
+                    orogen_model = self.orogen_model.class.new(Roby.app.default_orogen_project, nil)
+                    orogen_model.extended_state_support
+                    orogen_model.subclasses self.orogen_model
                 end
-                submodel.orogen_model = m
-
+                submodel.orogen_model = orogen_model
                 super
                 submodel.make_state_events
             end
