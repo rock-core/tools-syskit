9e4002f2434e4a32d7f9a5c1aa9c50f7aed391ef
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-04-25 14:19:17 -0300

    test: fix file nmaes in suite_robot

--- test/suite_robot.rb
+++ test/suite_robot.rb
@@ -2,6 +2,6 @@ require 'syskit/test/self'
 
 require './test/robot/test_device'
 require './test/robot/test_robot_definition'
-require './test/robot/test_master_device'
-require './test/robot/test_slave_device'
+require './test/robot/test_master_device_instance'
+require './test/robot/test_slave_device_instance'
 
