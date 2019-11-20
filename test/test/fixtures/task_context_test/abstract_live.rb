1b4d624c1f94bcadf6dd5b128807169defa56791
Sylvain <sylvain.joyeux@tidewise.io>
2019-11-20 13:53:45 -0300

    test: let the test writer(s) decide whether an abstract test should run live or not
    This boils down to the "subject model" concept in Roby/Syskit
    tests. In task context tests, Syskit tries to auto-define a
    deployment for the test subject, e.g.
    
    ~~~
    describe OroGen.iodrivers_base.ProxyTask do
    end
    ~~~
    
    This definition is not possible when in live mode for abstract
    models (e.g. iodrivers_base.Task). For this reason, the current
    spec implementation in Syskit was skipping these tests automatically.
    
    However, one can define test tasks based on these abstract models
    to test the abstract task functionality. In the case of these
    test components, we want the subject model to be the abstract
    task but run the tests live using a test task written for this
    purpose.
    
    Since Roby allows to define whether a test suite should run live
    or not, let the developer decide instead of enforcing the behavior
    deep down in the test implementation.

---
+++ test/test/fixtures/task_context_test/abstract_live.rb
@@ -0,0 +1,12 @@
+task_m = Syskit::TaskContext.new_submodel(name: 'some::Task') do
+    abstract
+end
+
+describe task_m do
+    run_live
+
+    it 'calls the tests even though the task is abstract' do
+        puts "TEST: CALLED"
+    end
+end
+
---
+++ test/test/test_task_context_test.rb
@@ -0,0 +1,54 @@
+require 'syskit/test/self'
+require 'syskit/test'
+
+require 'syskit/test/roby_app_helpers'
+
+module Syskit
+    module Test
+        describe TaskContextTest do
+            include Syskit::Test::RobyAppHelpers
+
+            describe 'abstract tasks' do
+                it 'auto-deploys an abstract task context when in abstract mode' do
+                    out = capture_test_output do
+                        dir = roby_app_setup_single_script
+                        run_test dir, 'abstract_simulation.rb'
+                    end
+                    assert_equal Set['TEST: CONFIGURE CALLED'], out
+                end
+
+                it 'lets live tests run' do
+                    out = capture_test_output do
+                        dir = roby_app_setup_single_script
+                        run_test dir, 'abstract_live.rb', '--live'
+                    end
+                    assert_equal Set['TEST: CALLED'], out
+                end
+            end
+
+            def run_test(dir, file, *args)
+                full = File.join(__dir__, 'fixtures', 'task_context_test', file)
+                assert roby_app_run('test', full, *args, chdir: dir).success?
+            end
+
+            def capture_test_output
+                error = nil
+                out, err = capture_subprocess_io do
+                    begin
+                        yield
+                    rescue Minitest::Assertion, StandardError => e
+                        error = e
+                    end
+                end
+
+                if error
+                    puts out
+                    puts err
+                    raise error
+                end
+
+                out.split("\n").map(&:chomp).grep(/^TEST: /).to_set
+            end
+        end
+    end
+end
