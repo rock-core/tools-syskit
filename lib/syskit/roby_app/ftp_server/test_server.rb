90f906d7ab8885248f1ef06d353e3a48ae6864e9
Roberto Marcos <roberto.marcos@poli.ufrj.br>
2021-02-01 17:33:32 -0300

    optimize tmp dir code logic

--- lib/syskit/roby_app/ftp_server/test_server.rb
+++ lib/syskit/roby_app/ftp_server/test_server.rb
@@ -1,6 +1,6 @@
 require "minitest/autorun"
 require "minitest/spec"
-require "tmpdir"
+
 require "ftpd"
 require "net/ftp"
 
@@ -20,16 +20,24 @@ describe Syskit::RobyApp::FtpServer do
 
         it "checks existence of ftp server" do
 
-            Dir.mktmpdir do |temp_dir|
-                server = Syskit::RobyApp::FtpServer::Server.new(temp_dir)
-                server.run
-                #FileUtils.touch File.expand_path('test.txt', temp_dir)
-                puts server.port
-                certificate = "/home/rbtmrcs/.local/share/autoproj/gems/ruby/2.5.0/gems/ftpd-2.1.0/insecure-test-cert.pem"
-                upload_log("127.0.0.1", "5000", certificate, ENV["LOGNAME"], "", "test.txt")
-                # assert (test, msg=nil)
-                assert File.exist?("#{temp_dir}/test.txt") != nil
-            end
+            temp_dir = Ftpd::TempDir.make
+            server = Syskit::RobyApp::FtpServer::Server.new(temp_dir)
+            assert Ftpd::DiskFileSystem::Accessors.exists?(server)
+
+        
+            # Dir.mktmpdir do |temp_dir|
+            #     server = Syskit::RobyApp::FtpServer::Server.new(temp_dir)
+            #     puts "\nServer running"
+            #     assert Net::FTP.closed? != false
+            #     assert Net::FTP.connect("127.0.0.1", port = server.port) != false
+                
+            #     #FileUtils.touch File.expand_path('test.txt', temp_dir)
+            #     certificate = "/home/rbtmrcs/.local/share/autoproj/gems/ruby/2.5.0/gems/ftpd-2.1.0/insecure-test-cert.pem"
+            #     upload_log("127.0.0.1", "5000", certificate, ENV["LOGNAME"], "", "test.txt")
+                
+            #     # assert (test, msg=nil)
+            #     assert File.exist?("#{temp_dir}/test.txt") != nil
+            # end
         end
 
     end
