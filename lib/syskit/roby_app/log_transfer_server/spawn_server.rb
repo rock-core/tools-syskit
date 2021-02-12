95521bf97dc179181e31e0e39c361721a97cdeaf
Mateus <mateusmacpedrosa@gmail.com>
2021-02-12 12:21:41 -0300

    Remove unnecessary attributes and method

--- lib/syskit/roby_app/log_transfer_server/spawn_server.rb
+++ lib/syskit/roby_app/log_transfer_server/spawn_server.rb
@@ -10,7 +10,7 @@ module Syskit
         module LogTransferServer
             # Class responsible for spawning an FTP server for transfering logs
             class SpawnServer
-                attr_reader :port, :interface
+                attr_reader :port
 
                 # tgt_dir must be an absolute path
                 def initialize(
@@ -18,7 +18,6 @@ module Syskit
                     user,
                     password,
                     certfile_path,
-                    account: "",
                     interface: "127.0.0.1",
                     tls: :explicit,
                     port: 0,
@@ -26,26 +25,21 @@ module Syskit
                     nat_ip: nil,
                     passive_ports: nil,
                     debug: false,
-                    log: nil,
                     verbose: false)
 
-                    @data_dir = tgt_dir
-                    @interface = interface
-                    @tls = tls
-                    @certfile_path = certfile_path
-                    @port = port
-                    @auth_level = "password"
-                    @user = user
-                    @password = password
-                    @account = account
-                    @session_timeout = session_timeout
-                    @nat_ip = nat_ip
-                    @passive_ports = passive_ports
                     @debug = debug
-                    @log = log
-                    @driver = Driver.new(@user, @password, @account, @data_dir)
-                    @server = Ftpd::FtpServer.new(@driver)
-                    configure_server
+                    driver = Driver.new(user, password, tgt_dir)
+                    server = Ftpd::FtpServer.new(driver)
+                    server.interface = interface
+                    server.port = port
+                    server.tls = tls
+                    server.passive_ports = passive_ports
+                    server.certfile_path = certfile_path
+                    server.auth_level = Ftpd.const_get("AUTH_PASSWORD")
+                    server.session_timeout = session_timeout
+                    server.log = make_log
+                    server.nat_ip = nat_ip
+                    @server = server
                     @server.start
                     @port = @server.bound_port
                     display_connection_info if verbose
@@ -58,36 +52,11 @@ module Syskit
 
                 private
 
-                def configure_server
-                    @server.interface = @interface
-                    @server.port = @port
-                    @server.tls = @tls
-                    @server.passive_ports = @passive_ports
-                    @server.certfile_path = @certfile_path
-                    @server.auth_level = auth_level
-                    @server.session_timeout = @session_timeout
-                    @server.log = make_log
-                    @server.nat_ip = @nat_ip
-                end
-
-                def auth_level
-                    Ftpd.const_get("AUTH_#{@auth_level.upcase}")
-                end
-
                 def display_connection_info
                     puts "Interface: #{@server.interface}"
                     puts "Port: #{@server.bound_port}"
-                    puts "User: #{@user.inspect}"
-                    puts "Pass: #{@password.inspect}" if auth_level >= Ftpd::AUTH_PASSWORD
-                    puts "Account: #{@account.inspect}" if auth_level >= Ftpd::AUTH_ACCOUNT
-                    puts "TLS: #{@tls}"
-                    puts "Directory: #{@data_dir}"
-                    puts "URI: #{uri}"
+                    puts "TLS: #{@server.tls}"
                     puts "PID: #{$$}"
-                end
-
-                def uri
-                    "ftp://#{connection_host}:#{@server.bound_port}"
                 end
 
                 def wait_until_stopped
@@ -102,15 +71,6 @@ module Syskit
 
                 def make_log
                     @debug && Logger.new($stdout)
-                end
-
-                def connection_host
-                    addr = IPAddr.new(@server.interface)
-                    if addr.ipv6?
-                        "::1"
-                    else
-                        "127.0.0.1"
-                    end
                 end
 
                 def default_session_timeout
