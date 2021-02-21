7c93a26e6c0b398350eea8158075fee8a4683321
Roberto Marcos <roberto.marcos@poli.ufrj.br>
2021-02-21 15:46:48 -0300

    adapt SpawnServer unit tests to later SpawnServer corrections
    It also removes unused entries in the .gitignore file

--- test/roby_app/spawn_server/test_spawn_server.rb
+++ test/roby_app/spawn_server/test_spawn_server.rb
@@ -17,41 +17,45 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
 
         def initialize(
             tgt_dir, 
-            user = "test.user", 
-            password = "test.password", 
+            user, 
+            password, 
             certfile_path = insecure_certfile_path)
             super
+            @user = user
+            @password = password
         end
     end
 
     ### AUXILIARY FUNCTIONS ###
     def spawn_server
         @temp_dir = Ftpd::TempDir.make
-        @server = TestServer.new(@temp_dir)
+        @user  = "test.user"
+        @password  = "test.password"
+        @server = TestServer.new(@temp_dir, @user, @password)
     end
 
     def upload_log(host, port, certificate, user, password, localfile)
         Net::FTP.open(host, port: port, verify_mode: OpenSSL::SSL::VERIFY_PEER, ca_file: certificate) do |ftp|
             ftp.login(user, password)
-            lf = File.open(localfile)
-            ftp.storbinary("STOR #{File.basename(localfile)}", lf, Net::FTP::DEFAULT_BLOCKSIZE)
-            lf.close
+            File.open(localfile) do |lf|
+                ftp.storbinary("STOR #{File.basename(localfile)}", lf, Net::FTP::DEFAULT_BLOCKSIZE)
+            end
         end
     end
 
     def upload_testfile
-        tf = File.open('testfile', 'w')
-        upload_log("127.0.0.1", @server.port, @server.certfile_path, @server.user, @server.password, tf)
-        tf.close
+        File.open("testfile", "w+") do |tf|
+            upload_log("127.0.0.1", @server.port, @server.certfile_path, @server.user, @server.password, tf)
+        end
     end
 
     ### TESTS ###
-    describe "#LogTransferServerTests" do
+    describe "#LogTransferServerTests" do     
 
-        before do
+        before do            
             spawn_server
         end
-        
+
         it "tests connection to server" do
             Net::FTP.open(
                 "127.0.0.1", 
@@ -63,28 +67,28 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
             end
         end
 
-        # it "tests file uploads to server" do
-        #     upload_testfile
-        #     assert File.exist?("#{@temp_dir}/testfile"), "Uploaded file doesn't exist."
-        # end
+        it "tests file uploads to server" do
+            upload_testfile
+            assert File.exist?("#{@temp_dir}/testfile"), "Uploaded file doesn't exist."
+        end
 
-        # it "tests upload of file that already exists" do
-        #     upload_testfile
-        #     assert_raises(Net::FTPPermError) {upload_testfile}
-        # end
+        it "tests upload of file that already exists" do
+            upload_testfile
+            assert_raises(Net::FTPPermError) {upload_testfile}
+        end
 
-        # it "tests read function blocking of remote repository" do
-        #     upload_testfile
-        #     Net::FTP.open(
-        #         "127.0.0.1", 
-        #         port: @server.port, 
-        #         verify_mode: OpenSSL::SSL::VERIFY_PEER, 
-        #         ca_file: @server.certfile_path) do |ftp|
+        it "tests read function blocking of remote repository" do
+            upload_testfile
+            Net::FTP.open(
+                "127.0.0.1", 
+                port: @server.port, 
+                verify_mode: OpenSSL::SSL::VERIFY_PEER, 
+                ca_file: @server.certfile_path) do |ftp|
                 
-        #         ftp.login(@server.user, @server.password)
-        #         assert_raises(Net::FTPPermError) { ftp.get("#{@temp_dir}/testfile") }
-        #     end
-        # end
+                ftp.login(@server.user, @server.password)
+                assert_raises(Net::FTPPermError) { ftp.get("#{@temp_dir}/testfile") }
+            end
+        end
 
     end
 end
