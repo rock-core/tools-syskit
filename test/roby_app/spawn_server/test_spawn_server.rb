fafe6e5a68acea8b94289580198f30513f7b8673
Sylvain <sylvain.joyeux@tidewise.io>
2021-03-18 09:48:04 -0300

    make sure test teardown is idempotent even in case of failures

--- test/roby_app/spawn_server/test_spawn_server.rb
+++ test/roby_app/spawn_server/test_spawn_server.rb
@@ -17,10 +17,10 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
 
     ### AUXILIARY FUNCTIONS ###
     def spawn_server
-        @temp_dir = Ftpd::TempDir.make
+        @temp_serverdir = make_tmpdir
         @user = "test.user"
         @password = "test.password"
-        @server = TestServer.new(@temp_dir, @user, @password)
+        @server = TestServer.new(@temp_serverdir, @user, @password)
     end
 
     def upload_log(host, port, certificate, user, password, localfile) # rubocop:disable Metrics/ParameterLists
@@ -35,32 +35,18 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
     end
 
     def upload_testfile
-        File.open("testfile", "w+") do |tf|
+        File.open(File.join(@temp_srcdir, "testfile"), "w+") do |tf|
             upload_log("127.0.0.1", @server.port, @server.certfile_path,
                        @server.user, @server.password, tf)
         end
-    end
-
-    def delete_testfile
-        File.delete(Dir.pwd + "/testfile")
     end
 
     ### TESTS ###
     describe "#LogTransferServerTests" do
         before do
             spawn_server
+            @temp_srcdir = make_tmpdir
         end
-
-        # WARNING: For debugging purposes only
-        # While debugging this code, the tests methods create files for
-        # testing. When executing in error, the code for deleting them
-        # may not do so.
-        # This 'after' method may optmize the clearing of these undesired files
-
-        # after do
-        #     File.delete("test_cert.pem")
-        #     File.delete("testfile")
-        # end
 
         it "tests connection to server" do
             Net::FTP.open(
@@ -111,19 +97,16 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
                 assert_raises(Net::FTPPermError) { ftp.login(@server.user, "password") }
             end
 
-            File.delete("test_cert.pem")
         end
 
         it "tests file uploads to server" do
             upload_testfile
             assert File.exist?("#{@temp_dir}/testfile"), "Uploaded file doesn't exist."
-            delete_testfile
         end
 
         it "tests upload of file that already exists" do
             upload_testfile
             assert_raises(Net::FTPPermError) { upload_testfile }
-            delete_testfile
         end
 
         it "tests read function blocking of remote repository" do
@@ -137,8 +120,6 @@ describe Syskit::RobyApp::LogTransferServer::SpawnServer do
                 ftp.login(@server.user, @server.password)
                 assert_raises(Net::FTPPermError) { ftp.get("#{@temp_dir}/testfile") }
             end
-            delete_testfile
         end
-
     end
 end
