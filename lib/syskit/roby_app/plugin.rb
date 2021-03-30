d7318c5c74d79c1738a8144928f783bd83961ace
rbtmrcs <roberto.marcos@tidewise.io>
2021-03-30 11:22:06 -0300

    correct port argument infile transfer client logic

--- lib/syskit/roby_app/plugin.rb
+++ lib/syskit/roby_app/plugin.rb
@@ -166,7 +166,7 @@ module Syskit
                     # Establishes communication with said process server
                     client = Syskit::RobyApp::RemoteProcesses::Client.new(
                             host: process_server[name].host_id,
-                            port: @log_transfer_server.port)
+                            port: @server_port)
                     # Commands method log_upload_file from said process server
                     client.log_upload_file(
                         host: "localhost", 
