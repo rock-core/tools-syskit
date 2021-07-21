8d6fc3dc618daaead947044592e66642a360cddb
Sylvain <sylvain.joyeux@tidewise.io>
2021-07-21 19:27:02 -0300

    chore: refactor update_task_states for rubocop and clarity

--- lib/syskit/runtime/update_task_states.rb
+++ lib/syskit/runtime/update_task_states.rb
@@ -1,87 +1,130 @@
 # frozen_string_literal: true
 
 module Syskit
-    module Runtime
-        # This method is called at the beginning of each execution cycle, and
-        # updates the running TaskContext tasks.
+    module Runtime # :nodoc:
+        # @api private
+        #
+        # Update the state of all unfinished tasks
+        #
+        # Called once per execution cycle by the engine
         def self.update_task_states(plan) # :nodoc:
-            query = plan.find_tasks(Syskit::TaskContext).not_finished
-            schedule = plan.execution_engine.scheduler.enabled?
-            query.each do |t|
-                execution_agent = t.execution_agent
-                # The task's deployment is not started yet
-                if !t.orocos_task
-                    plan.execution_engine.scheduler.report_holdoff "did not configure, execution agent not started yet", t
-                    next
-                elsif execution_agent.aborted_event.pending?
-                    next
-                elsif !execution_agent
-                    raise NotImplementedError, "#{t} is not yet finished but has no execution agent. #{t}'s history is\n  #{t.history.map(&:to_s).join("\n  ")}"
-                elsif !execution_agent.ready?
-                    raise InternalError, "orocos_task != nil on #{t}, but #{execution_agent} is not ready yet"
+            scheduler = plan.execution_engine.scheduler
+            plan.find_tasks(Syskit::TaskContext).not_finished.each do |task|
+                handle_single_task_state_update(scheduler, task)
+            end
+        end
+
+        # @api private
+        #
+        # Handle state changes for a single task
+        def self.handle_single_task_state_update(scheduler, task)
+            return unless handle_task_state_updates?(scheduler, task)
+
+            handle_task_configuration(scheduler, task) unless task.setup?
+            return if !task.running? && !task.starting? || task.aborted_event.pending?
+
+            handle_task_runtime_states(task)
+        end
+
+        # @api private
+        #
+        # Handle a task that needs to be configured
+        def self.handle_task_configuration(scheduler, task)
+            if task.setting_up?
+                scheduler.report_holdoff("is being configured", task)
+                return
+            end
+
+            return unless scheduler.enabled?
+            return unless task.meets_configurationg_precedence_constraints?
+
+            task.freeze_delayed_arguments
+            if task.will_never_setup?
+                unless task.kill_execution_agent_if_alone
+                    task.failed_to_start!(
+                        Roby::CommandFailed.new(
+                            InternalError.exception(
+                                "#{task} reports that it cannot be configured "\
+                                "(FATAL_ERROR ?)"
+                            ),
+                            task.start_event
+                        )
+                    )
                 end
+            elsif task.ready_for_setup?
+                task.setup.execute
+            else
+                scheduler.report_holdoff("did not configure, not ready for setup", task)
+            end
+        end
 
-                # Some CORBA implementations (namely, omniORB) may behave weird
-                # if the remote process terminates in the middle of a remote
-                # call.
-                #
-                # Ignore tasks whose process is terminating to reduce the
-                # likelihood of that happening
-                if execution_agent.finishing? || execution_agent.ready_to_die?
-                    next
-                end
+        # @api private
+        #
+        # Handle the runtime state changes coming from the oroGen component
+        def self.handle_task_runtime_states(task)
+            handled_this_cycle = []
 
-                if t.setting_up?
-                    plan.execution_engine.scheduler.report_holdoff "is being configured", t
-                end
+            state = nil
+            state_count = 0
+            while (!state || task.orocos_task.runtime_state?(state)) &&
+                  (new_state = task.update_orogen_state)
+                state_count += 1
 
-                if schedule && t.pending? && !t.setup? && !t.setting_up?
-                    next unless t.meets_configurationg_precedence_constraints?
-
-                    t.freeze_delayed_arguments
-                    if t.will_never_setup?
-                        unless t.kill_execution_agent_if_alone
-                            t.failed_to_start!(
-                                Roby::CommandFailed.new(
-                                    InternalError.exception("#{t} reports that it cannot be configured (FATAL_ERROR ?)"),
-                                    t.start_event
-                                )
-                            )
-                            next
-                        end
-                    elsif t.ready_for_setup?
-                        t.setup.execute
-                        next
-                    else
-                        plan.execution_engine.scheduler.report_holdoff "did not configure, not ready for setup", t
-                    end
-                end
-
-                next if !t.running? && !t.starting? || t.aborted_event.pending?
-
-                handled_this_cycle = []
-                begin
-                    state = nil
-                    state_count = 0
-                    while (!state || t.orocos_task.runtime_state?(state)) && (new_state = t.update_orogen_state)
-                        state_count += 1
-
-                        # Returns nil if we have a communication problem. In this
-                        # case, #update_orogen_state will have emitted the right
-                        # events for us anyway
-                        if new_state && handled_this_cycle.last != new_state
-                            t.handle_state_changes
-                            handled_this_cycle << new_state
-                        end
-                    end
-
-                    if state_count >= Deployment::STATE_READER_BUFFER_SIZE
-                        Runtime.warn "got #{state_count} state updates for #{t}, we might have lost some state updates in the process"
-                    end
-                rescue Orocos::CORBA::ComError => e
-                    t.aborted_event.emit e
+                # Returns nil if we have a communication problem. In this
+                # case, #update_orogen_state will have emitted the right
+                # events for us anyway
+                if handled_this_cycle.last != new_state
+                    task.handle_state_changes
+                    handled_this_cycle << new_state
                 end
             end
+
+            warn_state_reader_overrun(state_count)
+        rescue Orocos::CORBA::ComError => e
+            task.aborted_event.emit e
+        end
+
+        def self.warn_state_reader_overrun(state_count)
+            return if state_count < Deployment::STATE_READER_BUFFER_SIZE
+
+            Runtime.warn(
+                "got #{state_count} state updates for #{task}, we might "\
+                "have lost some state updates in the process"
+            )
+        end
+
+        # Check if the task is in a state that allow us to process its state updates
+        #
+        # Of notice, we stop looking at tasks when we know the underlying process is
+        # being killed. It has caused some deep freeze in OmniORB in the past.
+        def self.handle_task_state_updates?(scheduler, task)
+            execution_agent = task.execution_agent
+
+            # Check if the task is ready
+            if !task.orocos_task
+                scheduler.report_holdoff(
+                    "did not configure, execution agent not started yet", task
+                )
+                return
+            elsif !execution_agent
+                raise NotImplementedError,
+                      "#{task} is not yet finished but has no execution agent. "\
+                      "#{task}'s history is\n  #{task.history.map(&:to_s).join("\n  ")}"
+            elsif !execution_agent.ready?
+                raise InternalError,
+                      "orocos_task != nil on #{task}, "\
+                      "but #{execution_agent} is not ready yet"
+            end
+
+            # Some CORBA implementations (namely, omniORB) may behave weird
+            # if the remote process terminates in the middle of a remote
+            # call.
+            #
+            # Ignore tasks whose process is terminating to reduce the
+            # likelihood of that happening
+            return if execution_agent.finishing? || execution_agent.ready_to_die?
+
+            true
         end
     end
 end
