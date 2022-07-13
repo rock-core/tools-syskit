8d0ee5b7c6b79ebc6dce4ca20a72a56188f52e54
Sylvain <sylvain.joyeux@tidewise.io>
2022-07-13 10:34:13 -0300

    chore: fix keyword argument warnings

--- lib/syskit/models/component.rb
+++ lib/syskit/models/component.rb
@@ -608,9 +608,9 @@ module Syskit
             # dynamic service
             #
             # @see require_dynamic_service
-            def with_dynamic_service(dynamic_service_name, options = {})
+            def with_dynamic_service(dynamic_service_name, **options)
                 model = ensure_model_is_specialized
-                model.require_dynamic_service(dynamic_service_name, options)
+                model.require_dynamic_service(dynamic_service_name, **options)
                 model
             end
 
