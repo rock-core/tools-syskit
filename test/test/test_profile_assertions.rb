2b2c30cc3b3d1161fce88ad78dbb3a051aa4e0fa
Sylvain <sylvain.joyeux@tidewise.io>
2023-05-29 19:14:30 -0300

    fix: recursively expand coordination models in profile assertions

--- test/test/test_profile_assertions.rb
+++ test/test/test_profile_assertions.rb
@@ -573,6 +573,26 @@ module Syskit
                     assert_equal message, e.message
                 end
             end
+
+            describe ".each_combination" do
+                it "calculates and yields each possible combination of its arguments" do
+                    result = ProfileAssertions.each_combination(
+                        [1, 2, 3],
+                        [4, 5],
+                        [6, 7, 8]
+                    ).to_a
+
+                    expected = [
+                        [1, 4, 6], [1, 4, 7], [1, 4, 8],
+                        [1, 5, 6], [1, 5, 7], [1, 5, 8],
+                        [2, 4, 6], [2, 4, 7], [2, 4, 8],
+                        [2, 5, 6], [2, 5, 7], [2, 5, 8],
+                        [3, 4, 6], [3, 4, 7], [3, 4, 8],
+                        [3, 5, 6], [3, 5, 7], [3, 5, 8]
+                    ]
+                    assert_equal expected, result
+                end
+            end
         end
     end
 end
