72de283faa6dfbdba4a92dbeb0262819b24655d1
Sylvain <sylvain.joyeux@tidewise.io>
2023-07-04 11:05:43 -0300

    chore: move syskit/shell_interface into syskit/interface/commands
    To prepare for new parts for the interface.

---
+++ lib/syskit/interface/commands.rb
@@ -0,0 +1,253 @@
+# frozen_string_literal: true
+
+require "roby/interface/core"
+require "roby/robot"
+
+module Syskit
+    module Interface
+        # Definition of the syskit-specific interface commands
+        class Commands < Roby::Interface::CommandLibrary
+            # Save the configuration of all running tasks of the given model to disk
+            #
+            # @param [String,nil] name the section name for the new configuration.
+            #   If nil, the task's orocos name will be used
+            # @param [String] path the directory in which the files should be saved
+            # @return [nil]
+            def dump_task_config(task_model, path, name = nil)
+                FileUtils.mkdir_p(path)
+                plan.find_tasks(task_model)
+                    .each do |t|
+                        Orocos.conf.save(t.orocos_task, path, name || t.orocos_task.name)
+                    end
+                nil
+            end
+            command(
+                :dump_task_config,
+                "saves configuration from running tasks into yaml files",
+                model: "the model of the tasks that should be saved",
+                path: "the directory in which the configuration files should be saved",
+                name: "(optional) if given, the name of the section for the new "\
+                      "configuration. Defaults to the orocos task names"
+            )
+
+            # Saves the configuration of all running tasks to disk
+            #
+            # @param [String] name the section name for the new configuration
+            # @param [String] path the directory in which the files should be saved
+            # @return [nil]
+            def dump_all_config(path, name = nil)
+                dump_task_config(Syskit::TaskContext, path, name)
+                nil
+            end
+            command(
+                :dump_all_config,
+                "saves the configuration of all running tasks into yaml files",
+                path: "the directory in which the configuration files should be saved",
+                name: "(optional) if given, the name of the section for the new "\
+                      "configuration. Defaults to the orocos task names"
+            )
+
+            # Task used to tell the engine that we want the deployments restarted without
+            # killing the dependent networks
+            class ShellDeploymentRestart < Roby::Task
+                event :start, controlable: true
+
+                poll do
+                    if redeploy_event.pending? && !plan.syskit_has_async_resolution?
+                        redeploy_event.emit
+                    end
+                end
+
+                event :redeploy do |_context|
+                    Runtime.apply_requirement_modifications(plan, force: true)
+                end
+
+                forward redeploy: :stop
+            end
+
+            # Stops deployment processes
+            #
+            # @param [Array<Model<Deployment>>] models if non-empty, only the
+            #   deployments matching this model will be stopped, otherwise all
+            #   deployments are stopped.
+            def stop_deployments(*models)
+                models << Syskit::Deployment if models.empty?
+                models.each do |m|
+                    plan.find_tasks(m)
+                        .each do |task|
+                            if task.kind_of?(Syskit::TaskContext)
+                                task.execution_agent.stop!
+                            else
+                                task.stop!
+                            end
+                        end
+                end
+            end
+            command :stop_deployments, "stops deployment processes",
+                    models: "(optional) if given, a list of task or deployment models "\
+                            "pointing to what should be stopped. If not given, all "\
+                            "deployments are stopped"
+
+            # Restarts deployment processes
+            #
+            # @param [Array<Model<Deployment>,Model<TaskContext>>] models if
+            #   non-empty, only the deployments matching this model or the deployments
+            #   supporting tasks matching the models will be restarted, otherwise all
+            #   deployments are restarted.
+            def restart_deployments(*models)
+                models << Syskit::Deployment if models.empty?
+                deployments = restart_discover_deployment_tasks(models)
+                protection = restart_setup_protection(deployments)
+
+                done = Roby::AndGenerator.new
+                done.signals protection.redeploy_event
+                deployments.each do |task|
+                    done << task.stop_event
+                    task.stop!
+                end
+                nil
+            end
+            command :restart_deployments, "restarts deployment processes",
+                    models: "(optional) if given, a list of task or deployment models "\
+                    "pointing to what should be restarted. If not given, all "\
+                    "deployments are restarted"
+
+            # @api private
+            #
+            # Helper for {#restart_deployments} that lists the deployment tasks which
+            #   should be restarted
+            def restart_discover_deployment_tasks(models)
+                tasks = models.flat_map do |m|
+                    plan.find_tasks(m).map do |task|
+                        if task.kind_of?(Syskit::TaskContext)
+                            task.execution_agent
+                        else
+                            task
+                        end
+                    end
+                end
+                tasks.uniq
+            end
+
+            # @api private
+            #
+            # Helper for {#restart_deployments} that sets up a error handler to avoid
+            # killing tasks while we restart the deployments
+            def restart_setup_protection(deployment_tasks)
+                protection = ShellDeploymentRestart.new
+                plan.add(protection)
+                protection.start!
+
+                deployment_tasks.each do |task|
+                    task.each_executed_task do |executed_task|
+                        executed_task.stop_event.handle_with(protection)
+                    end
+                end
+                protection
+            end
+
+            # (see Application#syskit_reload_config)
+            def reload_config
+                app.syskit_reload_config
+            end
+            command :reload_config, "reloads YAML configuration files from disk",
+                    "You need to call the redeploy command to apply the new configuration"
+
+            # (see Application#syskit_pending_reloaded_configurations)
+            def pending_reloaded_configurations
+                app.syskit_pending_reloaded_configurations
+            end
+            command :pending_reloaded_configurations,
+                    "returns the list of TaskContext names "\
+                    "that are marked as needing reconfiguration",
+                    "They will be reconfigured on the next redeploy or system transition"
+
+            # Require the engine to redeploy the current network
+            #
+            # It must be called after {#reload_config} to apply the new
+            # configuration(s)
+            def redeploy
+                Runtime.apply_requirement_modifications(plan, force: true)
+                nil
+            end
+            command :redeploy, "redeploys the current network",
+                    "It is mostly used to apply the configuration "\
+                    "loaded with reload_config"
+
+            def enable_log_group(string)
+                Syskit.conf.logs.enable_log_group(string)
+                redeploy
+                nil
+            end
+            command :enable_logging_of, "enables a log group",
+                    name: "the log group name"
+
+            # @deprecated use enable_log_group instead
+            def enable_logging_of(string)
+                enable_log_group(string)
+            end
+
+            def disable_log_group(string)
+                Syskit.conf.logs.disable_log_group(string)
+                redeploy
+                nil
+            end
+            command :disable_log_group, "disables a log group",
+                    name: "the log group name"
+
+            # @deprecated use disable_log_group instead
+            def disable_logging_of(string)
+                disable_log_group(string)
+            end
+
+            LoggingGroup = Struct.new(:name, :enabled)
+            LoggingConfiguration =
+                Struct.new(:port_logs_enabled, :conf_logs_enabled, :groups)
+            def logging_conf
+                conf = LoggingConfiguration.new(false, false, {})
+                conf.port_logs_enabled = Syskit.conf.logs.port_logs_enabled?
+                conf.conf_logs_enabled = Syskit.conf.logs.conf_logs_enabled?
+                Syskit.conf.logs.groups.each_pair do |key, group|
+                    conf.groups[key] = LoggingGroup.new(key, group.enabled?)
+                end
+                conf
+            end
+            command :logging_conf, "gets the current logging configuration"
+
+            def update_logging_conf(conf)
+                logs_conf = Syskit.conf.logs
+                if conf.port_logs_enabled
+                    logs_conf.enable_port_logging
+                else
+                    logs_conf.disable_port_logging
+                end
+
+                if conf.conf_logs_enabled
+                    logs_conf.enable_conf_logging
+                else
+                    logs_conf.disable_conf_logging
+                end
+
+                conf.groups.each_pair do |name, group|
+                    logs_conf.group_by_name(name).enabled = group.enabled
+                rescue ArgumentError
+                    Syskit.warn "tried to update a group that does not exist: #{name}"
+                end
+                redeploy
+            end
+            command :update_logging_conf, "updates the current logging configuration",
+                    conf: "the new logging settings"
+        end
+    end
+end
+
+module Robot # :nodoc:
+    # Syskit subcommand for the shell
+    def self.syskit
+        @syskit_interface ||= Syskit::Interface::Commands.new(Roby.app) # rubocop:disable Naming/MemoizedInstanceVariableName
+    end
+end
+
+Roby::Interface::Interface.subcommand(
+    "syskit", Syskit::Interface::Commands, "Commands specific to Syskit"
+)
---
+++ test/interface/test_commands.rb
@@ -0,0 +1,263 @@
+# frozen_string_literal: true
+
+require "syskit/test/self"
+require "syskit/interface"
+
+module Syskit
+    module Interface
+        describe Commands do
+            attr_reader :subject
+
+            before do
+                @subject = Commands.new(flexmock(plan: plan))
+                subject.execution_engine.thread = Thread.current
+                @interface_thread = nil
+            end
+
+            after do
+                @ee_thread&.join
+            end
+
+            describe "#redeploy" do
+                it "triggers a full deployment" do
+                    flexmock(Runtime).should_receive(:apply_requirement_modifications)
+                                     .with(subject.plan, force: true).once.pass_thru
+                    subject.redeploy
+                end
+            end
+
+            describe "#restart_deployments" do
+                attr_reader :task_m, :task
+                before do
+                    @task_m = TaskContext.new_submodel
+                    @task = syskit_stub_deploy_configure_and_start(
+                        syskit_stub_requirements(task_m).with_conf("default")
+                    )
+                    plan.add_mission_task(task)
+                end
+
+                it "stops the matching deployments and redeploys" do
+                    plug_apply_requirement_modifications
+                    expect_execution do
+                        subject.restart_deployments
+                    end.to do
+                        emit find_tasks(Commands::ShellDeploymentRestart).stop_event
+                    end
+                    assert_equal 1, plan.find_tasks(task_m).pending.to_a.size
+                end
+
+                it "restricts the deployments to the given models" do
+                    other = syskit_stub_deploy_configure_and_start(
+                        syskit_stub_requirements(TaskContext.new_submodel)
+                    )
+                    plug_apply_requirement_modifications
+                    expect_execution do
+                        subject.restart_deployments(task.execution_agent.model)
+                    end.to do
+                        emit find_tasks(Commands::ShellDeploymentRestart).stop_event
+                        emit task.stop_event
+                        not_emit other.stop_event
+                    end
+                    assert_equal 1, plan.find_tasks(task_m).pending.to_a.size
+                end
+
+                it "accepts task models as argument" do
+                    other = syskit_stub_deploy_configure_and_start(
+                        syskit_stub_requirements(TaskContext.new_submodel)
+                    )
+                    plug_apply_requirement_modifications
+                    expect_execution do
+                        subject.restart_deployments(task.model)
+                    end.to do
+                        emit find_tasks(Commands::ShellDeploymentRestart).stop_event
+                        emit task.stop_event
+                        not_emit other.stop_event
+                    end
+                    assert_equal 1, plan.find_tasks(task_m).pending.to_a.size
+                end
+            end
+
+            describe "#stop_deployments" do
+                attr_reader :task_m, :task
+                before do
+                    @task_m = TaskContext.new_submodel
+                    @task = syskit_stub_deploy_configure_and_start(
+                        task_m.with_conf("default")
+                    )
+                    plan.add_mission_task(task)
+                end
+
+                it "stops the matching deployments" do
+                    expect_execution { subject.stop_deployments }
+                        .to do
+                            emit task.aborted_event
+                            emit task.execution_agent.stop_event
+                        end
+                    assert task.finished?
+                end
+
+                it "restricts the deployments to the given models" do
+                    other =
+                        syskit_stub_deploy_configure_and_start(task_m.with_conf("other"))
+                    subject.plan.add_mission_task(other)
+                    expect_execution do
+                        subject.stop_deployments(task.execution_agent.model)
+                    end.to do
+                        emit task.aborted_event
+                        emit task.execution_agent.stop_event
+                    end
+                    assert task.finished?
+                    assert !other.finished?
+                end
+
+                it "accepts task models as argument" do
+                    other_m = TaskContext.new_submodel
+                    other = syskit_stub_deploy_configure_and_start(other_m)
+                    subject.plan.add_mission_task(other)
+                    expect_execution do
+                        subject.stop_deployments(task.execution_agent.model)
+                    end.to do
+                        emit task.aborted_event
+                        emit task.execution_agent.stop_event
+                    end
+                    assert task.finished?
+                    assert !other.finished?
+                end
+            end
+
+            describe "the log configuration management" do
+                before { Syskit.conf.logs.create_group "test" }
+                after { Syskit.conf.logs.remove_group("test") }
+
+                it "creates a marshallable instance of the configuration" do
+                    conf = subject.logging_conf
+                    assert_equal conf.port_logs_enabled,
+                                 Syskit.conf.logs.port_logs_enabled?
+                    assert_equal conf.conf_logs_enabled,
+                                 Syskit.conf.logs.conf_logs_enabled?
+                    Syskit.conf.logs.groups.each_pair do |key, group|
+                        assert_equal group.enabled?, conf.groups[key].enabled
+                    end
+                    Marshal.dump(conf)
+                end
+
+                it "changes status of conf and port logging and redeploys" do
+                    conf = subject.logging_conf
+                    previous_port_status = Syskit.conf.logs.port_logs_enabled?
+                    previous_conf_status = Syskit.conf.logs.conf_logs_enabled?
+
+                    conf.port_logs_enabled = !previous_port_status
+                    conf.conf_logs_enabled = !previous_conf_status
+
+                    flexmock(subject).should_receive(:redeploy).once.pass_thru do
+                        assert_equal Syskit.conf.logs.port_logs_enabled?,
+                                     !previous_port_status
+                        assert_equal Syskit.conf.logs.conf_logs_enabled?,
+                                     !previous_conf_status
+                    end
+                    subject.update_logging_conf(conf)
+                end
+
+                it "changes status of an existing log group and redeploys" do
+                    conf = subject.logging_conf
+                    previous_status = Syskit.conf.logs.group_by_name("test").enabled?
+                    conf.groups["test"].enabled = !previous_status
+
+                    flexmock(subject).should_receive(:redeploy).once.pass_thru do
+                        assert_equal Syskit.conf.logs.group_by_name("test").enabled?,
+                                     !previous_status
+                    end
+                    subject.update_logging_conf(conf)
+                end
+            end
+
+            describe "the log group management" do
+                attr_reader :group
+                before do
+                    @group = Syskit.conf.logs.create_group "test" do |g|
+                        g.add(/base.samples.frame.Frame/)
+                    end
+                end
+
+                after do
+                    Syskit.conf.logs.remove_group("test")
+                end
+
+                it "enable_log_group enables the log group and redeploys" do
+                    group.enabled = false
+                    flexmock(subject).should_receive(:redeploy).once.ordered
+                    subject.enable_log_group "test"
+                    assert group.enabled?
+                end
+
+                it "disable_log_group enables the log group and redeploys" do
+                    group.enabled = true
+                    flexmock(subject).should_receive(:redeploy).once.ordered
+                    subject.disable_log_group "test"
+                    assert !group.enabled?
+                end
+
+                it "enable_log_group raises ArgumentError "\
+                   "if the log group does not exist" do
+                    assert_raises(ArgumentError) do
+                        subject.enable_log_group "does_not_exist"
+                    end
+                end
+
+                it "disable_log_group raises ArgumentError "\
+                   "if the log group does not exist" do
+                    assert_raises(ArgumentError) do
+                        subject.disable_log_group "does_not_exist"
+                    end
+                end
+            end
+
+            # Start a thread to do a call on the interface that is synchronized
+            # with {ExecutionEngine#execute}
+            #
+            # @example call the 'redeploy' interface command, and wait for its
+            #   result
+            #
+            #   # Call 'redeploy'. The queue method waits 50ms to give time to
+            #   # the thread to actually start the call. There's no way to be
+            #   # sure, so that might lead to random test failures
+            #   queue_execute_call { subject.redeploy }
+            #   # Force processing of the call, and wait for the thread to
+            #   # finish
+            #   process_execute_call
+            def queue_execute_call(&block)
+                if @interface_thread
+                    raise "you must call #process_execute_call after a call "\
+                          "to #queue_execute_call"
+                end
+
+                @interface_thread_sync = sync = Concurrent::CyclicBarrier.new(2)
+                @interface_thread = Thread.new do
+                    sync.wait
+                    block.call
+                    sync.wait
+                end
+                sync.wait
+                sleep 0.05
+            end
+
+            # Process the work queued with {#queue_execute_call}
+            def process_execute_call
+                subject.execution_engine.join_all_waiting_work
+                if !@interface_thread.alive?
+                    # Join the thread to have it to raise an exception that
+                    # would have terminated it
+                    @interface_thread.join
+                    # If no exception was risen, fail with a less helpful
+                    # message
+                    flunck("interface thread quit unexpectedly")
+                else
+                    @interface_thread_sync.wait
+                    @interface_thread.join
+                end
+            ensure
+                @interface_thread = nil
+            end
+        end
+    end
+end
