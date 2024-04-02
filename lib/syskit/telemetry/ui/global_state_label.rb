eb048508d303e0a940e076571fa4f126c03c8339
Sylvain <sylvain.joyeux@tidewise.io>
2024-04-02 13:12:51 -0300

    chore: copy the runtime state view from gui/ into telemetry/ui/
    This is only code copy and change of namespaces. No other changes.

---
+++ lib/syskit/telemetry/ui/global_state_label.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+module Syskit
+    module Telemetry
+        module UI
+            # Representation of the state of the connected Roby instance
+            class GlobalStateLabel < StateLabel
+                # Actions that are shown when the context menu is activated
+                #
+                # @return [Array<Qt::Action>] the list of actions that can be
+                #   performed on the remote Roby instance (e.g. start/stop ...)
+                attr_reader :actions
+
+                # @param [Array<Qt::Action>] the list of actions that can be
+                #   performed on the remote Roby instance
+                def initialize(actions: [], **options)
+                    super(extra_style: "margin-left: 2px; margin-top: 2px; font-size: 10pt;",
+                          rate_limited: true, **options)
+                    @actions = actions
+                    declare_state "STARTING", :blue
+                    declare_state "RESTARTING", :blue
+                    declare_state "CONNECTED", :green
+                    declare_state "UNREACHABLE", :red
+                end
+
+                # @api private
+                #
+                # Qt handler called when the context menu is activated
+                def contextMenuEvent(event)
+                    unless actions.empty?
+                        app_state_menu(event.global_pos)
+                        event.accept
+                    end
+                end
+
+                # Execute the app state menu
+                def app_state_menu(global_pos)
+                    unless actions.empty?
+                        menu = Qt::Menu.new(self)
+                        actions.each { |act| menu.add_action(act) }
+                        menu.exec(global_pos)
+                        true
+                    end
+                end
+
+                # @api private
+                #
+                # Qt handler called when the mouse is pressed
+                def mousePressEvent(event)
+                    event.accept
+                end
+
+                # @api private
+                #
+                # Qt handler called when the mouse is released
+                #
+                # It emits the 'clicked' signal
+                def mouseReleaseEvent(event)
+                    emit clicked(event.global_pos)
+                    event.accept
+                end
+                signals "clicked(QPoint)"
+            end
+        end
+    end
+end
---
+++ lib/syskit/telemetry/ui/logging_configuration.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+require "vizkit"
+require "vizkit/vizkit_items"
+require "vizkit/tree_view"
+require "Qt4"
+require "syskit/interface"
+require "syskit/telemetry/ui/logging_configuration_item"
+require "roby/interface/exceptions"
+
+module Syskit
+    module Telemetry
+        module UI
+            # A widget containing an editable TreeView to allow the user to
+            # manage basic Syskit's logging configuration
+            class LoggingConfiguration < Qt::Widget
+                attr_reader :model, :tree_view, :syskit, :item_name, :item_value, :pending_call
+                def initialize(syskit, parent = nil)
+                    super(parent)
+                    main_layout = Qt::VBoxLayout.new(self)
+                    @tree_view = Qt::TreeView.new
+
+                    Vizkit.setup_tree_view tree_view
+                    @model = Vizkit::VizkitItemModel.new
+                    tree_view.setModel @model
+                    main_layout.add_widget(tree_view)
+                    tree_view.setColumnWidth(0, 200)
+                    tree_view.style_sheet = <<~STYLESHEET
+                        QTreeView {
+                            background-color: rgb(255, 255, 219);
+                            alternate-background-color: rgb(255, 255, 174);
+                            color: rgb(0, 0, 0);
+                        }
+                        QTreeView:disabled { color: rgb(159, 158, 158); }
+                    STYLESHEET
+
+                    @syskit = syskit
+                    @timer = Qt::Timer.new
+                    @timer.connect(SIGNAL("timeout()")) { refresh }
+                    @timer.start 1500
+
+                    refresh
+                end
+
+                # Whether there is a refreshing call pending
+                def refreshing?
+                    syskit.async_call_pending?(pending_call)
+                end
+
+                # Fetches the current logging configuration from syskit's
+                # sync interface
+                def refresh
+                    if syskit.reachable?
+                        begin
+                            return if refreshing?
+
+                            @pending_call = syskit.async_call ["syskit"], :logging_conf do |error, result|
+                                if error.nil?
+                                    enabled true
+                                    update_model(result)
+                                else
+                                    enabled false
+                                end
+                            end
+                        rescue Roby::Interface::ComError
+                            enabled false
+                        end
+                    else
+                        enabled false
+                    end
+                end
+
+                # Expands the entire tree
+                def recursive_expand(item)
+                    tree_view.expand(item.index)
+                    (0...item.rowCount).each do |i|
+                        recursive_expand(item.child(i))
+                    end
+                end
+
+                # Changes the top most item in the tree state
+                # and makes it update its childs accordingly
+                def enabled(toggle)
+                    @item_name&.enabled toggle
+                end
+
+                # Updates the view model
+                def update_model(conf)
+                    if @item_name.nil?
+                        @item_name = LoggingConfigurationItem.new(conf, :accept => true)
+                        @item_value = LoggingConfigurationItem.new(conf)
+                        @item_value.setEditable true
+                        @item_value.setText ""
+                        @model.appendRow([@item_name, @item_value])
+                        recursive_expand(@item_name)
+
+                        @item_name.on_accept_changes do |new_conf|
+                            begin
+                                syskit.async_call ["syskit"], :update_logging_conf, new_conf do |error, result|
+                                    enabled false unless error.nil?
+                                end
+                            rescue Roby::Interface::ComError
+                                enabled false
+                            end
+                        end
+                    else
+                        return if @item_name.modified?
+
+                        @item_name.update_conf(conf)
+                    end
+                end
+            end
+        end
+    end
+end
