f75a5c10d24ff156396c8b9a5844a28e2aee8959
Sylvain <sylvain.joyeux@tidewise.io>
2024-04-07 07:29:48 -0300

    feat: add GPRC scaffold

---
+++ test/telemetry/agent/test_client.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "syskit/test/self"
+require "syskit/telemetry/agent/client"
+
+module Syskit
+    module Telemetry
+        module Agent
+            describe Client do
+            end
+        end
+    end
+end
