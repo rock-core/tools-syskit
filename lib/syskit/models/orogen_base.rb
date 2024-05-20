5da920fb2251eea869c8bde375e4f400f8215838
Sylvain <sylvain.joyeux@tidewise.io>
2024-05-20 15:35:16 -0300

    fix: allow making OrogenBase#model_for strict
    The current implementation was not detecting ambiguities, which is
    why some bugs would appear (essentially things were functioning more
    because of magic)
    
    Setting the strict_model_for configuration flag allows to make sure
    that this situation would not go undetected, but since it will break
    existing code it needs to be enabled explicitly.

--- lib/syskit/models/orogen_base.rb
+++ lib/syskit/models/orogen_base.rb
@@ -20,6 +20,18 @@ module Syskit
                 nil
             end
 
+            # Return all syskit models subclasses of self that represent the
+            # given oroGen model
+            #
+            # @param orogen_model the oroGen model
+            # @return [Array<Syskit::TaskContext,Syskit::Deployment>] the
+            #   corresponding syskit model, or nil if there are none registered
+            def find_all_models_by_orogen(orogen_model)
+                each_submodel.find_all do |syskit_model|
+                    syskit_model.orogen_model == orogen_model
+                end
+            end
+
             # Return the syskit model that represents the given oroGen model
             #
             # @param orogen_model the oroGen model
@@ -34,13 +46,34 @@ module Syskit
                 nil
             end
 
-            # Returns the syskit model for the given oroGen model
+            # Returns the syskit model subclass of self for the given oroGen model
             #
             # @raise ArgumentError if no syskit model exists
+            # @raise ArgumentError if more than one matching syskit model exists,
+            #   and Conf.syskit.strict_model_for is set to true
             def model_for(orogen_model)
-                if m = find_model_by_orogen(orogen_model)
+                if Syskit.conf.strict_model_for?
+                    strict_model_for(orogen_model)
+                elsif (m = find_model_by_orogen(orogen_model))
                     m
-                else raise ArgumentError, "there is no syskit model for #{orogen_model.name}"
+                else
+                    raise ArgumentError, "there is no Syskit model for #{orogen_model.name}"
+                end
+            end
+
+            # @api private
+            #
+            # Implementation of {#model_for} when Syskit.conf.strict_model_for is set
+            def strict_model_for(orogen_model)
+                matches = find_all_models_by_orogen(orogen_model)
+                if matches.size == 1
+                    matches.first
+                elsif matches.empty?
+                    raise ArgumentError,
+                          "there is no Syskit model for #{orogen_model.name}"
+                else
+                    raise ArgumentError,
+                          "more than one Syskit model matches #{orogen_model.name}"
                 end
             end
         end
