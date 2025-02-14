b8903e33850cddcb3e844a5a931e3fc557ea56b4
eduardacoppo <eduarda.coppo@tidewise.io>
2025-02-14 15:33:25 -0300

    fix: do not merge if no init policy

--- lib/syskit/network_generation/dataflow_dynamics.rb
+++ lib/syskit/network_generation/dataflow_dynamics.rb
@@ -620,7 +620,10 @@ module Syskit
                 end
 
                 source_port_m = source_port.model
-                policy.merge(init: source_port_m.init_policy)
+                unless source_port_m.init_policy.nil?
+                    policy.merge(init: source_port_m.init_policy)
+                end
+                policy
             end
 
             def compute_reliable_connection_policy(
