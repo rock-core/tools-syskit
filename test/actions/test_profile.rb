e8ef3ca94ddfc9fcdb10276b2991a1138fa82985
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2025-05-29 17:20:36 -0300

    fix: only instanciate toplevel abstract tasks when instanciating placeholders
    Placeholders would instanciate a whole composition when based on a composition
    model

--- test/actions/test_profile.rb
+++ test/actions/test_profile.rb
@@ -354,22 +354,40 @@ module Syskit
             end
 
             describe "#tag" do
-                it "cannot be merged with another tag of the same type" do
-                    srv_m = DataService.new_submodel
-                    profile = Profile.new
-                    profile.tag "test", srv_m
-                    profile.tag "other", srv_m
-                    test_task = profile.test_tag.instanciate(plan)
-                    other_task = profile.other_tag.instanciate(plan)
-                    refute test_task.can_merge?(other_task)
-                end
-                it "can be merged with another instance of itself" do
-                    srv_m = DataService.new_submodel
-                    profile = Profile.new
-                    profile.tag "test", srv_m
-                    test_task1 = profile.test_tag.instanciate(plan)
-                    test_task2 = profile.test_tag.instanciate(plan)
-                    assert test_task1.can_merge?(test_task2)
+                describe "a tag instance" do
+                    it "cannot be merged with an instance of another tag " \
+                       "of the same type" do
+                        srv_m = DataService.new_submodel
+                        profile = Profile.new
+                        profile.tag "test", srv_m
+                        profile.tag "other", srv_m
+                        test_task = profile.test_tag.instanciate(plan)
+                        other_task = profile.other_tag.instanciate(plan)
+                        refute test_task.can_merge?(other_task)
+                    end
+
+                    it "can be merged with another instance of itself" do
+                        srv_m = DataService.new_submodel
+                        profile = Profile.new
+                        profile.tag "test", srv_m
+                        test_task1 = profile.test_tag.instanciate(plan)
+                        test_task2 = profile.test_tag.instanciate(plan)
+                        assert test_task1.can_merge?(test_task2)
+                    end
+
+                    it "overrides component model instanciation to always only add " \
+                       "an abstract placeholder task" do
+                        task_m = TaskContext.new_submodel
+                        cmp_m = Composition.new_submodel
+                        cmp_m.add task_m, as: "child"
+
+                        profile = Profile.new
+                        profile.tag "test", cmp_m
+                        task = profile.test_tag.instanciate(plan)
+                        assert task.abstract?
+                        assert task.placeholder?
+                        refute task.find_child_from_role("child")
+                    end
                 end
             end
 
